0332f741793321457a03a951d523f59aa376e528
Vijay Raghunathan <vr@bsb.in>
2011-12-09 16:10:33 +0530

    make our pubsub system accessible from a singleton

--- src/com/bsb/hike/HikeMessengerApp.java
+++ src/com/bsb/hike/HikeMessengerApp.java
@@ -1,5 +1,8 @@
 package com.bsb.hike;
 
+import com.bsb.hike.HikePubSub.Listener;
+import com.bsb.hike.utils.DbConversationListener;
+
 import android.app.Application;
 
 public class HikeMessengerApp extends Application {
@@ -8,8 +11,20 @@ public class HikeMessengerApp extends Application {
 	public static final String CARRIER_SETTING = "carrier";
 	public static final String NAME_SETTING = "name";
 	public static final String TOKEN_SETTING = "token";
+	private static HikePubSub mPubSubInstance;
+	static {
+		mPubSubInstance = new HikePubSub();
+	}
+
 	public void onCreate() {
 		super.onCreate();
+		HikePubSub pubSub = HikeMessengerApp.getPubSub();
+		Listener listener = new DbConversationListener(getApplicationContext());
+		pubSub.addListener(HikePubSub.MESSAGE, listener);
+	}
+
+	public static HikePubSub getPubSub() {
+		return mPubSubInstance;
 	}
 
 }
