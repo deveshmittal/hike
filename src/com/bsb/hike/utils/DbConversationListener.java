a72c5bcbc566906168faffa80d4a0521b94546f1
Vijay Raghunathan <vr@bsb.in>
2011-12-11 13:22:01 +0530

    Persist all sorts of messages

--- src/com/bsb/hike/utils/DbConversationListener.java
+++ src/com/bsb/hike/utils/DbConversationListener.java
@@ -2,6 +2,7 @@ package com.bsb.hike.utils;
 
 import android.content.Context;
 
+import com.bsb.hike.HikePubSub;
 import com.bsb.hike.HikePubSub.Listener;
 import com.bsb.hike.models.ConvMessage;
 
@@ -17,9 +18,13 @@ public class DbConversationListener implements Listener {
 
 	@Override
 	public void onEventReceived(String type, Object object) {
-		if (type.equals("message")) {
+		if (HikePubSub.MESSAGE_SENT.equals(type)) {
 			ConvMessage message = (ConvMessage) object;
 			mConversationDb.addConversation(message);
+		} else if (HikePubSub.MESSAGE_RECEIVED.equals(type)) {
+			ConvMessage message = (ConvMessage) object;
+			mConversationDb.addConversation(message);
+			//TODO update the unread flags here
 		}
 	}
 
