e608c04db9abfea2369d03ebc55869868193cc69
Vijay Raghunathan <vr@bsb.in>
2011-12-14 18:49:29 +0530

    It appears that we can fail to lookup a number for a contact that we know is in the phone and know has a number.  Crazy

--- src/com/bsb/hike/utils/ContactUtils.java
+++ src/com/bsb/hike/utils/ContactUtils.java
@@ -12,8 +12,9 @@ public class ContactUtils {
 	 * Gets the mobile number for a contact.  If there's no mobile number, gets the default one
 	 */
 	public static String getMobileNumber(ContentResolver cr, Uri contact) {
-		Cursor cursor = cr.query(contact,null, null, null, null);
-		if (!cursor.moveToFirst()) {
+		Cursor cursor = cr.query(contact, new String[]{"_id"}, null, null, null);
+		if ((cursor == null) || 
+			(!cursor.moveToFirst())) {
 			Log.w("ContactUtils", "No contact found: " + contact);
 			return null;
 		}
@@ -21,6 +22,10 @@ public class ContactUtils {
 		String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
 		Cursor phones = cr.query(Phone.CONTENT_URI, null, Phone.CONTACT_ID + " = " + contactId, null, null);
 		String number = null;
+		if (phones == null) {
+			return null;
+		}
+
 		while (phones.moveToNext()) {
 			number = phones.getString(phones.getColumnIndex(Phone.NUMBER));
 			int type = phones.getInt(phones.getColumnIndex(Phone.TYPE));
