4318d58f522351ce4a3ba6d24773a6fe5fce93fa
Vijay Raghunathan <vr@bsb.in>
2011-12-21 18:37:19 +0530

    Remove unused imports

--- src/com/bsb/hike/adapters/MessagesAdapter.java
+++ src/com/bsb/hike/adapters/MessagesAdapter.java
@@ -1,11 +1,8 @@
 package com.bsb.hike.adapters;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
