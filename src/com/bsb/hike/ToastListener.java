7b4be8ae11a59d51d35744ae612f809734b3b02a
Vijay Raghunathan <vr@bsb.in>
2012-01-04 21:19:39 +0530

    Refactor out the toasting part of the Toast listener so we reuse it elsewhere

--- src/com/bsb/hike/ToastListener.java
+++ src/com/bsb/hike/ToastListener.java
@@ -3,34 +3,24 @@ package com.bsb.hike;
 import java.lang.ref.WeakReference;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.text.Html;
-import android.text.Spanned;
 
 import com.bsb.hike.HikePubSub.Listener;
-import com.bsb.hike.models.ContactInfo;
 import com.bsb.hike.models.ConvMessage;
 import com.bsb.hike.ui.ChatThread;
 import com.bsb.hike.ui.MessagesList;
-import com.bsb.hike.utils.HikeUserDatabase;
+import com.bsb.hike.utils.HikeToast;
 
 public class ToastListener implements Listener {
 
-	private static final int NEW_MESSAGE_ID = 1;
-
-	private Context context;
 	private WeakReference<Activity> currentActivity;
-	private NotificationManager notificationManager;
+
+    private HikeToast toaster;
 
 	public ToastListener(Context ctx) {
-		this.context = ctx;
 		HikeMessengerApp.getPubSub().addListener(HikePubSub.MESSAGE_RECEIVED, this);
 		HikeMessengerApp.getPubSub().addListener(HikePubSub.NEW_ACTIVITY, this);
-		notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		this.toaster = new HikeToast(ctx);
 	}
 
 	@Override
@@ -51,35 +41,8 @@ public class ToastListener implements Listener {
 				return;
 			}
 
-			/* the foreground activity isn't going to show this message so Toast it */
-			HikeUserDatabase db = new HikeUserDatabase(context);
-			ContactInfo contactInfo = db.getContactInfoFromMSISDN(message.getMsisdn());
-			db.close();
-
-			String name = (contactInfo != null) ? contactInfo.name : message.getMsisdn();
-			int icon = R.drawable.ic_launcher;
-
-			//TODO this doesn't turn the text bold :(
-			Spanned text = Html.fromHtml(String.format("<bold>%1$s</bold>:%2$s", name, message.getMessage()));
-			Notification notification = new Notification(icon, text, message.getTimestamp());
-			notification.flags = notification.flags | Notification.FLAG_AUTO_CANCEL;
-
-			Intent notificationIntent = new Intent(context, ChatThread.class);
-			notificationIntent.putExtra("msisdn", message.getMsisdn());
-
-			if (contactInfo != null) {
-				if (contactInfo.id != null) {
-					notificationIntent.putExtra("id", Long.parseLong(contactInfo.id));
-				}
-				if (contactInfo.name != null) {
-					notificationIntent.putExtra("name", contactInfo.name);
-				}
-			}
-
-			PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
-
-			notification.setLatestEventInfo(context, name, message.getMessage(), contentIntent);
-			notificationManager.notify(NEW_MESSAGE_ID, notification);
+	         /* the foreground activity isn't going to show this message so Toast it */
+			this.toaster.toast(message.getMsisdn(), message.getMessage(), message.getTimestamp());
 		}
 	}
 
---
+++ src/com/bsb/hike/utils/HikeToast.java
@@ -0,0 +1,57 @@
+package com.bsb.hike.utils;
+
+import com.bsb.hike.R;
+import com.bsb.hike.models.ContactInfo;
+import com.bsb.hike.ui.ChatThread;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.text.Html;
+import android.text.Spanned;
+
+public class HikeToast {
+    private static final int NEW_MESSAGE_ID = 1;
+
+    private Context context;
+    private NotificationManager notificationManager;
+
+    public HikeToast(Context context) {
+        this.context = context;
+        this.notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    public void toast(String msisdn, String message, int timestamp) {
+
+        HikeUserDatabase db = new HikeUserDatabase(context);
+        ContactInfo contactInfo = db.getContactInfoFromMSISDN(msisdn);
+        db.close();
+
+        String name = (contactInfo != null) ? contactInfo.name : msisdn;
+        int icon = R.drawable.ic_launcher;
+
+        //TODO this doesn't turn the text bold :(
+        Spanned text = Html.fromHtml(String.format("<bold>%1$s</bold>:%2$s", name, msisdn));
+        Notification notification = new Notification(icon, text, timestamp);
+        notification.flags = notification.flags | Notification.FLAG_AUTO_CANCEL;
+
+        Intent notificationIntent = new Intent(context, ChatThread.class);
+        notificationIntent.putExtra("msisdn", msisdn);
+
+        if (contactInfo != null) {
+            if (contactInfo.id != null) {
+                notificationIntent.putExtra("id", Long.parseLong(contactInfo.id));
+            }
+            if (contactInfo.name != null) {
+                notificationIntent.putExtra("name", contactInfo.name);
+            }
+        }
+
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        notification.setLatestEventInfo(context, name, message, contentIntent);
+        notificationManager.notify(NEW_MESSAGE_ID, notification);        
+    }
+}
