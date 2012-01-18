3777f721b04509b25709f725a8a14ed2bf20c5f4
Gautam Kanwar <gk@bsb.in>
2012-01-18 16:18:47 +0530

    Included some logs to track the flow of the code, also changed the update function.
    Conflicts:
    
    	src/com/bsb/hike/utils/DbConversationListener.java

--- src/com/bsb/hike/utils/DbConversationListener.java
+++ src/com/bsb/hike/utils/DbConversationListener.java
@@ -39,14 +39,16 @@ public class DbConversationListener implements Listener
 		{
 			ConvMessage convMessage = (ConvMessage) object;
 			mConversationDb.addConversationMessages(convMessage);
+			Log.i("MESSAGE SENT","sender msg id -> "+convMessage.getMsgID() + " ; receiver msg id -> "+convMessage.getMappedMsgID()+" ; MsgStatus -> "+convMessage.getState().name());
 			mPubSub.publish(HikePubSub.WS_SEND, convMessage.serialize("send")); // this is used to be sent by the web socket.
 		}
 		else if (HikePubSub.MESSAGE_RECEIVED_FROM_SENDER.equals(type))  // represents event when a client receive msg from other client through server.
 		{
 			ConvMessage message = (ConvMessage) object;
 			mConversationDb.addConversationMessages(message);
-			mPubSub.publish(HikePubSub.MESSAGE_RECEIVED, message);
+			Log.d("MESSAGE RECEIVED","receiver msg id -> "+message.getMsgID() + " ; sender msg id -> "+message.getMappedMsgID()+" ; MsgStatus -> "+message.getState().name());
 			mPubSub.publish(HikePubSub.WS_SEND, message.serializeDeliveryReport("msgDelivered")); // handle return to sender
+			mPubSub.publish(HikePubSub.MESSAGE_RECEIVED, message);		
 		}
 		else if (HikePubSub.SMS_CREDIT_CHANGED.equals(type))
 		{
@@ -56,15 +58,18 @@ public class DbConversationListener implements Listener
 		} 
 		else if (HikePubSub.SERVER_RECEIVED_MSG.equals(type))  // server got msg from client 1 and sent back received msg receipt
 		{
+			Log.d("MSG RECEIPT","Msg confirmed by server for msgId -> "+(Long)object);
 			updateDB(object,ConvMessage.State.SENT_CONFIRMED.ordinal());
 		}
 		else if (HikePubSub.MESSAGE_DELIVERED.equals(type))  // server got msg from client 1 and sent back received msg receipt
 		{
+			Log.d("MSG DELIVERY REPORT","Msg delivered to receiver for msgID -> "+(Long)object);
 			updateDB(object,ConvMessage.State.SENT_DELIVERED.ordinal());
 		}
 		
 		else if (HikePubSub.MESSAGE_DELIVERED_READ.equals(type))  // server got msg from client 1 and sent back received msg receipt
 		{
+			Log.d("MSG DELIVERY REPORT","Msg Read by receiver for msgID -> "+(Long)object);
 			updateDB(object,ConvMessage.State.SENT_DELIVERED_READ.ordinal());
 		}
 		
@@ -74,7 +79,8 @@ public class DbConversationListener implements Listener
 	{
 		long msgID = (Long)object;
 		/* TODO we should lookup the convid for this user, since otherwise one could set mess with the state for other conversations */
-		int rowsAffected = mConversationDb.updateMsgStatus(0,msgID,status);
+		int rowsAffected = mConversationDb.updateMsgStatus(msgID,status);
+		Log.d("UPDATE SUCCESS","Rows Affected -> "+rowsAffected);
 		if(rowsAffected<=0) // signifies no msg.
 		{
 			Log.e("DbConversationListener", "Could not update msgstatus for message: " + msgID);
