4b506052618ebf2d609eeddd7043d3f4126b2742
Vijay Raghunathan <vr@bsb.in>
2012-01-21 12:40:08 +0530

    Implements hashCode and equals so we can use these objects in Sets

--- src/com/bsb/hike/models/ContactInfo.java
+++ src/com/bsb/hike/models/ContactInfo.java
@@ -21,6 +21,51 @@ public class ContactInfo
 		this(id, number, name, false);
 	}
 
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContactInfo other = (ContactInfo) obj;
+		if (id == null)
+		{
+			if (other.id != null)
+				return false;
+		}
+		else if (!id.equals(other.id))
+			return false;
+		if (name == null)
+		{
+			if (other.name != null)
+				return false;
+		}
+		else if (!name.equals(other.name))
+			return false;
+		if (number == null)
+		{
+			if (other.number != null)
+				return false;
+		}
+		else if (!number.equals(other.number))
+			return false;
+		return true;
+	}
+
 	public ContactInfo(String id, String number, String name, boolean onhike)
 	{
 		this.id = id;
