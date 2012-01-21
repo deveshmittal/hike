5bfaa14e177ac4bf05a055d1e1c88d48f6275489
Vijay Raghunathan <vr@bsb.in>
2012-01-21 14:31:13 +0530

    Generic compressed HTTP entity encoder.  Moved to a new package

---
+++ src/com/bsb/hike/http/GzipByteArrayEntity.java
@@ -0,0 +1,40 @@
+package com.bsb.hike.http;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.zip.GZIPOutputStream;
+
+import org.apache.http.entity.ByteArrayEntity;
+
+public class GzipByteArrayEntity extends ByteArrayEntity
+{
+	public GzipByteArrayEntity(final byte[] data, final String encoding) throws UnsupportedEncodingException
+	{
+		super(gzip(data, encoding));
+		setContentType("application/json");
+		setContentEncoding("gzip");
+	}
+
+	private static byte[] gzip(byte[] data, String encoding)
+	{
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		GZIPOutputStream zos;
+		try
+		{
+			zos = new GZIPOutputStream(bos);
+			zos.write(data);
+			zos.close();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+		return bos.toByteArray();
+	}
+
+
+}
--- src/com/bsb/hike/http/GzipUrlEncodedFormEntity.java
+++ src/com/bsb/hike/http/GzipUrlEncodedFormEntity.java
@@ -1,4 +1,4 @@
-package com.bsb.hike.utils;
+package com.bsb.hike.http;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
