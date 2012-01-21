ad30772c640ec82958d9809387c1ef8c2a53a0e3
Vijay Raghunathan <vr@bsb.in>
2012-01-21 14:44:21 +0530

    Utility functions to serialize json

--- src/com/bsb/hike/utils/Utils.java
+++ src/com/bsb/hike/utils/Utils.java
@@ -2,6 +2,15 @@ package com.bsb.hike.utils;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+import com.bsb.hike.models.utils.JSONSerializable;
 
 public class Utils
 {
@@ -19,5 +28,34 @@ public class Utils
 			builder.append(delimiter);
 		}
 		return builder.toString();
+	}
+
+	/* serializes the given collection into an object.
+	 * Ignores exceptions
+	 */
+	public static JSONArray ajsonSerialize(Collection<? extends JSONSerializable> elements)
+	{
+		JSONArray arr = new JSONArray();
+		for (JSONSerializable elem : elements)
+		{
+			try
+			{
+				arr.put(elem.toJSON());
+			} catch (JSONException e)
+			{
+				Log.e("Utils", "error json serializing", e);
+			}
+		}
+		return arr;
+	}
+
+	public static JSONObject sjsonSerialize(Map<String, ? extends JSONSerializable> elements) throws JSONException
+	{
+		JSONObject obj = new JSONObject();
+		for (Map.Entry<String, ? extends JSONSerializable> element : elements.entrySet())
+		{
+			obj.put(element.getKey(), element.getValue().toJSON());
+		}
+		return obj;
 	}
 }
