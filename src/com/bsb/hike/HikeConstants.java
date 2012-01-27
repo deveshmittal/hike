83446c7b77b0a8026cc8c16f679c53eb2d1a14fb
Vijay Raghunathan <vr@bsb.in>
2012-01-27 11:33:55 +0530

    Add some constants

--- src/com/bsb/hike/HikeConstants.java
+++ src/com/bsb/hike/HikeConstants.java
@@ -3,4 +3,8 @@ package com.bsb.hike;
 public class HikeConstants
 {
 	public static final String MESSAGE = "msg";
+	public static final String UI_TOPIC = "/u";
+	public static final String APP_TOPIC = "/u";
+	public static final String SERVICE_TOPIC = "/u";
+	public static final String PUBLISH_TOPIC = "/p";
 }
