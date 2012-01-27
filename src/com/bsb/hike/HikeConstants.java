9f87d68a4e732282117a2677d9d6904f64443f04
Vijay Raghunathan <vr@bsb.in>
2012-01-27 12:13:08 +0530

    Typo in constants

--- src/com/bsb/hike/HikeConstants.java
+++ src/com/bsb/hike/HikeConstants.java
@@ -4,7 +4,7 @@ public class HikeConstants
 {
 	public static final String MESSAGE = "msg";
 	public static final String UI_TOPIC = "/u";
-	public static final String APP_TOPIC = "/u";
-	public static final String SERVICE_TOPIC = "/u";
+	public static final String APP_TOPIC = "/a";
+	public static final String SERVICE_TOPIC = "/s";
 	public static final String PUBLISH_TOPIC = "/p";
 }
