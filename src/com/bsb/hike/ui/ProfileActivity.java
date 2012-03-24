a8cdee473cbbb2926f4044e2577d6963b4d37376
Vijay Raghunathan <vr@bsb.in>
2012-03-24 16:01:58 +0530

    Add an edit icon that toggles the edit state of the profile screen

--- src/com/bsb/hike/ui/ProfileActivity.java
+++ src/com/bsb/hike/ui/ProfileActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -19,7 +22,7 @@ import com.bsb.hike.adapters.ProfileArrayAdapter;
 import com.bsb.hike.models.ProfileItem;
 import com.bsb.hike.models.utils.IconCacheManager;
 
-public class ProfileActivity extends Activity implements OnItemClickListener
+public class ProfileActivity extends Activity implements OnItemClickListener, OnClickListener
 {
 
 	private ImageView mIconView;
@@ -27,6 +30,10 @@ public class ProfileActivity extends Activity implements OnItemClickListener
 	private ListView mListView;
 	private TextView mTitleView;
 	private TextView mMadeWithLoveView;
+	private ImageView mTitleIcon;
+	private View mProfilePictureChangeOverlay;
+	private boolean mEditable = false; /* is this page currently editable */
+	private EditText mNameViewEdittable;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -36,9 +43,12 @@ public class ProfileActivity extends Activity implements OnItemClickListener
 
 		mIconView = (ImageView) findViewById(R.id.profile);
 		mNameView = (TextView) findViewById(R.id.name);
+		mNameViewEdittable = (EditText) findViewById(R.id.name_editable);
 		mListView = (ListView) findViewById(R.id.profile_preferences);
 		mTitleView = (TextView) findViewById(R.id.title);
+		mTitleIcon = (ImageView) findViewById(R.id.title_icon);
 		mMadeWithLoveView = (TextView) findViewById(R.id.made_with_love);
+		mProfilePictureChangeOverlay = findViewById(R.id.profile_change_overlay);
 
 		Drawable drawable = IconCacheManager.getInstance().getIconForMSISDN(HikeConstants.ME);
 		mIconView.setImageDrawable(drawable);
@@ -47,7 +57,11 @@ public class ProfileActivity extends Activity implements OnItemClickListener
 		String name = settings.getString(HikeMessengerApp.NAME, "Set a name!");
 		mNameView.setText(name);
 
+		mNameView.setOnClickListener(this);
+
 		mTitleView.setText(getResources().getString(R.string.profile_title));
+		mTitleIcon.setImageResource(R.drawable.ic_editmessage);
+		mTitleIcon.setVisibility(View.VISIBLE);
 
 		/* add the heart in code because unicode isn't supported via xml*/
 		//mMadeWithLoveView.setText(String.format(getString(R.string.made_with_love), "\u2665"));
@@ -73,6 +87,35 @@ public class ProfileActivity extends Activity implements OnItemClickListener
 		if (intent != null)
 		{
 			startActivity(intent);			
+		}
+	}
+
+	public void onTitleIconClick(View view)
+	{
+		mEditable = !mEditable;
+		if (mEditable)
+		{
+			mNameViewEdittable.setText(mNameView.getText());
+		}
+		else
+		{
+			/* save the new fields */
+		}
+
+		mProfilePictureChangeOverlay.setVisibility(mEditable ? View.VISIBLE : View.GONE);
+		mNameViewEdittable.setVisibility(mEditable ? View.VISIBLE : View.GONE);
+		mNameView.setVisibility(!mEditable ? View.VISIBLE : View.GONE);
+	}
+
+	@Override
+	public void onClick(View view)
+	{
+		if (view == mNameView)
+		{
+			mNameView.setVisibility(View.GONE);
+			EditText editableTextView = (EditText) findViewById(R.id.name_editable);
+			editableTextView.setVisibility(View.VISIBLE);
+			editableTextView.requestFocus();
 		}
 	}
 
