77694999675cecfbe24edaf3b9faae16d842861d
Vijay Raghunathan <vr@bsb.in>
2012-03-24 14:38:43 +0530

    Use custom layout everywhere for preferences

--- src/com/bsb/hike/view/IconCheckBoxPreference.java
+++ src/com/bsb/hike/view/IconCheckBoxPreference.java
@@ -10,55 +10,39 @@ import android.widget.ImageView;
 import com.bsb.hike.R;
 
 public class IconCheckBoxPreference extends CheckBoxPreference {
-    private Drawable mIcon;
-    
-    public IconCheckBoxPreference(final Context context, final AttributeSet attrs, final int defStyle) {
+	private Drawable mIcon;
+
+	public IconCheckBoxPreference(final Context context, final AttributeSet attrs, final int defStyle) {
         super(context, attrs, defStyle);
+        setIcon(context, attrs);
+	}
 
-        this.setLayoutResource(R.layout.icon_checkbox_preference);
-
+    private void setIcon(Context context, AttributeSet attrs)
+	{
         String iconName = attrs.getAttributeValue(null, "icon");
-        Log.d("IconCheckBoxPreference", "iconName is " + iconName);
 
-        int id = context.getResources().getIdentifier(iconName, null, context.getPackageName());
+        int id = context.getResources().getIdentifier(iconName, "drawable", context.getPackageName());
+    
         this.mIcon = context.getResources().getDrawable(id);
-//        this.mIcon = context.getDrawable(R.styleable.IconPreference_icon);
+	}
+
+	public IconCheckBoxPreference(Context context)
+	{
+		super(context);
+	}
+
+	public IconCheckBoxPreference(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+		setIcon(context, attrs);
     }
 
-    public IconCheckBoxPreference(final Context context, final AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    @Override
     protected void onBindView(final View view) {
         super.onBindView(view);
-        final ImageView imageView = (ImageView)view.findViewById(R.id.icon);
-        Log.d("IconCheckBoxPreference", "in onBindView " + imageView + " " + this.mIcon);
-        if ((imageView != null) && (this.mIcon != null)) {
-            imageView.setImageDrawable(this.mIcon);
+        final ImageView imageView = (ImageView) view.findViewById(R.id.icon);
+        if ((imageView != null) && (this.mIcon != null))
+        {
+        	imageView.setImageDrawable(this.mIcon);
+        	imageView.setVisibility(View.VISIBLE);
         }
-    }
-
-    /**
-     * Sets the icon for this Preference with a Drawable.
-     *
-     * @param icon The icon for this Preference
-     */
-    public void setIcon(final Drawable icon) {
-    	Log.d("IconCheckBoxPreference", "setIcon " + icon);
-        if (((icon == null) && (this.mIcon != null)) || ((icon != null) && (!icon.equals(this.mIcon)))) {
-            this.mIcon = icon;
-            this.notifyChanged();
-        }
-    }
-
-    /**
-     * Returns the icon of this Preference.
-     *
-     * @return The icon.
-     * @see #setIcon(Drawable)
-     */
-    public Drawable getIcon() {
-        return this.mIcon;
     }
 }
