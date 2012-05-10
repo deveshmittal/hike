d5c2955fb63882339a666796add7921c9fdbe530
Rishabh Sharma <rs@bsb.in>
2012-05-10 23:58:23 +0530

    Changed the icons of the preference screens

--- src/com/bsb/hike/view/IconCheckBoxPreference.java
+++ src/com/bsb/hike/view/IconCheckBoxPreference.java
@@ -3,7 +3,6 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.preference.CheckBoxPreference;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -11,6 +10,7 @@ import com.bsb.hike.R;
 
 public class IconCheckBoxPreference extends CheckBoxPreference {
 	private Drawable mIcon;
+	private ImageView imageView;
 
 	public IconCheckBoxPreference(final Context context, final AttributeSet attrs, final int defStyle) {
         super(context, attrs, defStyle);
@@ -38,11 +38,23 @@ public class IconCheckBoxPreference extends CheckBoxPreference {
 
     protected void onBindView(final View view) {
         super.onBindView(view);
-        final ImageView imageView = (ImageView) view.findViewById(R.id.icon);
+        imageView = (ImageView) view.findViewById(R.id.icon);
         if ((imageView != null) && (this.mIcon != null))
         {
         	imageView.setImageDrawable(this.mIcon);
         	imageView.setVisibility(View.VISIBLE);
+        	imageView.setSelected(isChecked());
         }
     }
+
+	@Override
+	protected void notifyChanged() {
+		if(imageView != null)
+		{
+			imageView.setSelected(isChecked());
+		}
+		super.notifyChanged();
+	}
+    
+    
 }
