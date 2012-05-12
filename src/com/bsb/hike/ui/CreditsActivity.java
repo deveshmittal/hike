013e1ca6f6d05e6f9d99c4b27be7f4b844a5dfe3
Rishabh Sharma <rs@bsb.in>
2012-05-12 10:39:38 +0530

    Changed the credits activity UI. The complete screen is smaller in length now (TODO: make the invite number dynamic)

--- src/com/bsb/hike/ui/CreditsActivity.java
+++ src/com/bsb/hike/ui/CreditsActivity.java
@@ -3,7 +3,12 @@ package com.bsb.hike.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -23,17 +28,46 @@ public class CreditsActivity extends Activity
 	private TextView creditsNum;
 	private Button inviteFriendsBtn;
 	private SharedPreferences settings;
+	private int numHike;
+	private TextView impTxt;
+	private TextView friendsNumTxt;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.credits);
-
+		
+		numHike = 0;
+		
 		mTitleView = (TextView) findViewById(R.id.title);
 		creditItemContainer = (LinearLayout) findViewById(R.id.credit_item_container);
 		creditsNum = (TextView) findViewById(R.id.credit_no);
 		inviteFriendsBtn = (Button) findViewById(R.id.invite_now);
+		impTxt = (TextView) findViewById(R.id.imp_txt);
+		friendsNumTxt = (TextView) findViewById(R.id.friends_num);
+
+		String imp = getString(R.string.important);
+		String dnd = getString(R.string.dnd);
+		SpannableString s = new SpannableString(imp + " " + dnd);
+
+		s.setSpan(new ForegroundColorSpan(0xffff3333), 0, imp.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+		int ind = dnd.indexOf("10 SMS");
+		int l = new String("10 SMS").length();
+		s.setSpan(new StyleSpan(Typeface.BOLD), imp.length() + ind, imp.length() + ind + l + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+		impTxt.setText(s);
+
+		String formatString = getResources().getString(R.string.friends_on_hike_0);
+		String num = Integer.toString(numHike);
+		String formatted = String.format(formatString, num);
+		
+		SpannableString str = new SpannableString(formatted);
+		int start = formatString.indexOf("%1$s");
+		str.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), start,
+				start + num.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		friendsNumTxt.setText(str);
 		
 		settings = getSharedPreferences(HikeMessengerApp.ACCOUNT_SETTINGS, 0);
 		int credits = settings.getInt(HikeMessengerApp.SMS_SETTING, 0);
@@ -49,8 +83,21 @@ public class CreditsActivity extends Activity
 			TextView smsNum = (TextView) v.findViewById(R.id.sms_no);
 			
 			int smsNo = 100 + (i*20);
+
+			friendNum.setText(i + "");
+			if (i == 5) 
+			{
+				friendNum.setText(i + "+");
+				if(numHike >= 5)
+				{
+					v.setBackgroundResource(R.drawable.credit_item_bckg_selected);
+				}
+			}
+			else if(i == numHike)
+			{
+				v.setBackgroundResource(R.drawable.credit_item_bckg_selected);
+			}
 			
-			friendNum.setText(i+"");
 			smsNum.setText(smsNo+"");
 			creditItemContainer.addView(v);
 		}
