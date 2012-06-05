f7f51c687b8de4676b17d75e5934c80bdc81fb74
Vijay Raghunathan <vr@bsb.in>
2012-06-05 13:22:20 +0530

    Require at least 3 lines before showing the SMS Credits

--- src/com/bsb/hike/ui/ChatThread.java
+++ src/com/bsb/hike/ui/ChatThread.java
@@ -1195,7 +1195,7 @@ public class ChatThread extends Activity implements HikePubSub.Listener, TextWat
 				hideOverlay();
 			}
 
-			if(mComposeView.getLineCount()>1)
+			if(mComposeView.getLineCount()>2)
 			{
 				mMetadataNumChars.setVisibility(View.VISIBLE);
 				int length = mComposeView.getText().length();
