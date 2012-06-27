ad749e2f6b6a5f0302fff359a189b3d5d87beb32
Rishabh Sharma <rs@bsb.in>
2012-06-27 19:20:36 +0530

    Changed the number after which the tool tips show up

--- src/com/bsb/hike/HikeConstants.java
+++ src/com/bsb/hike/HikeConstants.java
@@ -66,7 +66,7 @@ public class HikeConstants
 	public static final int NO_UPDATE = 0;
 	
 	// More explanation required?
-	public static final int NUM_TIMES_SCREEN_SHOULD_OPEN_BEFORE_TOOL_TIP = 4;
+	public static final int NUM_TIMES_SCREEN_SHOULD_OPEN_BEFORE_TOOL_TIP = 2;
 
 	public static int NUM_SMS_PER_FRIEND = 10;
 	public static int INITIAL_NUM_SMS = 100;
