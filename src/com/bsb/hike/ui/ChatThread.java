edd7865bdc2fa357adc8af07b14d38ed6c85a1be
Rishabh Sharma <rs@bsb.in>
2012-07-24 12:05:09 +0530

    Fixed bug where the user could still send emoticons and text even after the group had ended

--- src/com/bsb/hike/ui/ChatThread.java
+++ src/com/bsb/hike/ui/ChatThread.java
@@ -802,6 +802,7 @@ public class ChatThread extends Activity implements HikePubSub.Listener, TextWat
 		findViewById(R.id.button_bar_2).setVisibility(View.GONE);
 
 		mComposeView.setFocusable(true);
+		mComposeView.setVisibility(View.VISIBLE);
 		mComposeView.requestFocus();
 		/* hide the number picker */
 		mInputNumberView.setVisibility(View.GONE);
@@ -1785,7 +1786,8 @@ public class ChatThread extends Activity implements HikePubSub.Listener, TextWat
 
 	private void groupChatDead()
 	{
-		this.mComposeView.setEnabled(false);
+		this.mComposeView.setVisibility(View.INVISIBLE);
 		this.titleIconView.setEnabled(false);
+		findViewById(R.id.emo_btn).setEnabled(false);
 	}
 }
