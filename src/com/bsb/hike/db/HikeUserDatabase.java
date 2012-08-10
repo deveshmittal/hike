1f7d0d816f87e51197f5b9fda47e5c174d1f36e6
Rishabh Sharma <rs@bsb.in>
2012-08-10 13:47:33 +0530

    This method should be selecting using the phone number and not msisdn

--- src/com/bsb/hike/db/HikeUserDatabase.java
+++ src/com/bsb/hike/db/HikeUserDatabase.java
@@ -478,7 +478,7 @@ public class HikeUserDatabase extends SQLiteOpenHelper
 	public ContactInfo getContactInfoFromPhoneNo(String number)
 	{
 		Cursor c = mReadDb.query(DBConstants.USERS_TABLE, new String[] { DBConstants.MSISDN, DBConstants.ID, DBConstants.NAME, DBConstants.ONHIKE,DBConstants.PHONE, DBConstants.MSISDN_TYPE, DBConstants.LAST_MESSAGED, DBConstants.HAS_CUSTOM_PHOTO }, 
-												DBConstants.MSISDN + "=?", new String[] { number }, null, null, null);
+												DBConstants.PHONE + "=?", new String[] { number }, null, null, null);
 		List<ContactInfo> contactInfos = extractContactInfo(c);
 		c.close();
 		if (contactInfos.isEmpty())
