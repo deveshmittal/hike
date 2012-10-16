fcab8295d202ba20e834771d30d65d61c2d77fd6
Rishabh Sharma <rs@bsb.in>
2012-10-16 14:46:49 +0530

    Added fiksu library

---
+++ src/com/fiksu/asotracking/RatingEventTracker.java
@@ -0,0 +1,27 @@
+/*
+ * Android ASO Tracking Code
+ *
+ * Version: 1.2
+ *
+ * Copyright (C) 2011 Fiksu Incorporated
+ * All Rights Reserved. 
+ *
+ */
+
+package com.fiksu.asotracking;
+
+import android.content.Context;
+
+import com.fiksu.asotracking.EventTracker;
+
+class RatingEventTracker extends EventTracker {
+	RatingEventTracker(Context context, String outcome, int launches) {
+		super(context, "Rating");
+		addParameter("tvalue", outcome);
+		addParameter("ivalue", new Integer(launches).toString());
+	}
+	
+	public void uploadEvent() {
+		super.uploadEvent();
+	}
+}
