2d759b227bf8dfbfa3abba3dbdc39c0889bcca31
Rishabh Sharma <rs@bsb.in>
2012-11-06 17:44:39 +0530

    Showing non hike contacts in the recents list as well

--- src/com/bsb/hike/view/DrawerLayout.java
+++ src/com/bsb/hike/view/DrawerLayout.java
@@ -181,7 +181,7 @@ public class DrawerLayout extends RelativeLayout implements View.OnClickListener
 		List<ContactInfo> completeList = new ArrayList<ContactInfo>();
 
 		List<ContactInfo> favoriteList = hikeUserDatabase.getContactsOrderedByLastMessaged(-1, 1, false, true);
-		List<ContactInfo> recentList = hikeUserDatabase.getContactsOrderedByLastMessaged(HikeConstants.RECENT_COUNT_IN_FAVORITE, 0, true, true);
+		List<ContactInfo> recentList = hikeUserDatabase.getContactsOrderedByLastMessaged(HikeConstants.RECENT_COUNT_IN_FAVORITE, 0, false, true);
 	
 		//Contact for "Favorite Section"
 		completeList.add(new ContactInfo(DrawerFavoritesAdapter.FAVORITES_SECTION_ID, null, HikeConstants.FAVORITES, null));
