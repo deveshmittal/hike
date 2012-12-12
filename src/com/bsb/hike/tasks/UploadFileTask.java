fb4b19b86f9d13f052b213ca58aaba9d4e7a9813
Rishabh Sharma <rs@bsb.in>
2012-12-12 17:28:43 +0530

    Removed unused import

--- src/com/bsb/hike/tasks/UploadFileTask.java
+++ src/com/bsb/hike/tasks/UploadFileTask.java
@@ -1,10 +1,6 @@
 package com.bsb.hike.tasks;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.json.JSONArray;
