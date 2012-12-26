280ad187db5dd9aca81e5c019679dc08c8140ea4
Rishabh Sharma <rs@bsb.in>
2012-12-26 14:58:53 +0530

    We should unregister and register to the GCM service when the app is updated

--- src/com/bsb/hike/service/AppUpdatedReceiver.java
+++ src/com/bsb/hike/service/AppUpdatedReceiver.java
@@ -16,6 +16,7 @@ import com.bsb.hike.HikeMessengerApp;
 import com.bsb.hike.HikePubSub;
 import com.bsb.hike.db.HikeUserDatabase;
 import com.bsb.hike.utils.Utils;
+import com.google.android.gcm.GCMRegistrar;
 
 /**
  * @author Rishabh This receiver is used to notify that the app has been
@@ -102,6 +103,14 @@ public class AppUpdatedReceiver extends BroadcastReceiver {
 				HikeMessengerApp.getPubSub().publish(
 						HikePubSub.FREE_SMS_TOGGLED, freeSMSOn);
 			}
+
+			/*
+			 * Unregister and ask for a new GCM token
+			 */
+			Log.d(getClass().getSimpleName(),
+					"Unregistering from GCM since the app was updated");
+			GCMRegistrar.unregister(context);
+			context.sendBroadcast(new Intent(HikeService.REGISTER_TO_GCM_ACTION));
 		}
 	}
 }
