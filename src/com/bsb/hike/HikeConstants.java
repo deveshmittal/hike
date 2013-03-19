558b103d4a8759943ac529b60f58275adc4fbc32
Rishabh Sharma <rs@bsb.in>
2013-03-19 14:23:22 +0530

    Added a preference to turn on/off status update notifications

--- src/com/bsb/hike/HikeConstants.java
+++ src/com/bsb/hike/HikeConstants.java
@@ -98,6 +98,7 @@ public class HikeConstants {
 	public static final String LED_PREF = "ledPref";
 	public static final String NATIVE_JINGLE_PREF = "jinglePref";
 	public static final String SSL_PREF = "sslPref";
+	public static final String STATUS_PREF = "statusPref";
 	public static final String HIKEBOT = "TD-HIKE";
 
 	public static final String DONE = "Done";
