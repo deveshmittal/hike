4016c6d5783a82e592e183360a37ab35dc17df63
Rishabh Sharma <rs@bsb.in>
2013-03-20 22:41:03 +0530

    We first close the mood/emoji picker if the back button is pressed while the status dialog is open

--- src/com/bsb/hike/view/StatusDialog.java
+++ src/com/bsb/hike/view/StatusDialog.java
@@ -3,6 +3,8 @@ package com.bsb.hike.view;
 import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.bsb.hike.R;
 
@@ -14,12 +16,17 @@ public class StatusDialog extends Dialog {
 
 	@Override
 	public void onBackPressed() {
-		View v = findViewById(R.id.mood_parent);
-		if (v != null && v.getVisibility() == View.VISIBLE) {
-			v.setVisibility(View.GONE);
+		View v1 = findViewById(R.id.mood_parent);
+		View v2 = findViewById(R.id.emoji_container);
+		if ((v1 != null && v1.getVisibility() == View.VISIBLE)
+				|| (v2 != null && v2.getVisibility() == View.VISIBLE)) {
+			v1.setVisibility(View.GONE);
+			v2.setVisibility(View.GONE);
+			Button post = ((Button) findViewById(R.id.title_icon));
+			post.setText(R.string.post);
+			post.setEnabled(((EditText) findViewById(R.id.status_txt)).length() > 0);
 			return;
 		}
 		super.onBackPressed();
 	}
-
 }
