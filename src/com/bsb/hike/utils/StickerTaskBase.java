773d0ace5edaebf3aca78dad6d76997853408d68
Rishabh Sharma <rs@bsb.in>
2013-06-05 15:24:30 +0530

    Formatting change

--- src/com/bsb/hike/utils/StickerTaskBase.java
+++
@@ -1,9 +0,0 @@
-package com.bsb.hike.utils;
-
-import com.bsb.hike.HikeConstants.FTResult;
-
-import android.os.AsyncTask;
-
-public abstract class StickerTaskBase extends AsyncTask<Void, Void, FTResult> {
-
-}
