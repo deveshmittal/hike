8baec3fcbf0b09b2e93970b461e2a16b8df04ccb
Pankaj Verma <pankajv@bsb.in>
2013-09-22 23:16:46 +0530

    updated native sms popup and added a checked preferences to not show the popup again

--- src/com/bsb/hike/HikeConstants.java
+++ src/com/bsb/hike/HikeConstants.java
@@ -184,6 +184,7 @@ public class HikeConstants {
 	public static final String UPGRADE_AVATAR_PROGRESS_USER = "upgradeAvtarProgressUser";
 	public static final String UPGRADE_AVATAR_CONV_DB ="upgradeAvtarProgressConv";
 	public static final String NO_CALL_ALERT_CHECKED ="noCallAlertChecked";
+	public static final String OPERATOR_SMS_ALERT_CHECKED = "opSmsAlertChecked";
 	public static final String IS_GAMES_ITEM_CLICKED ="isGamesItemClicked";
 	public static final String IS_REWARDS_ITEM_CLICKED ="isRewardsItemClicked";
 	public static final String IS_OF_ICON_CLICKED ="isOfIconClicked";
