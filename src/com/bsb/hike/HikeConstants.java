5393da560843cb32bec0361c5684aaadba0eea38
Rishabh Sharma <rs@bsb.in>
2013-09-26 02:16:40 +0530

    Formatting change

--- src/com/bsb/hike/HikeConstants.java
+++ src/com/bsb/hike/HikeConstants.java
@@ -177,7 +177,7 @@ public class HikeConstants {
 	public static final String LAST_SEEN_PREF = "lastSeenPref";
 	public static final String AUTO_DOWNLOAD_IMAGE_PREF = "autoDownloadImagePref";
 	public static final String HIKEBOT = "TD-HIKE";
-	public static final String HIKEBOT_CONV_STATE ="isHikeBotConvState";
+	public static final String HIKEBOT_CONV_STATE = "isHikeBotConvState";
 	public static final String FTUE_HIKEBOT_MSISDN = "+Hike1+";
 	public static final String UNLINK_PREF = "unlinkAccount";
 	public static final String DELETE_PREF = "deleteAccount";
@@ -190,14 +190,14 @@ public class HikeConstants {
 	public static final String STATUS_BOOLEAN_PREF = "statusBooleanPref";
 	public static final String UPGRADE_AVATAR_PROGRESS_USER = "upgradeAvtarProgressUser";
 	public static final String UPGRADE_AVATAR_CONV_DB = "upgradeAvtarProgressConv";
-	public static final String NO_CALL_ALERT_CHECKED ="noCallAlertChecked";
+	public static final String NO_CALL_ALERT_CHECKED = "noCallAlertChecked";
 	public static final String OPERATOR_SMS_ALERT_CHECKED = "opSmsAlertChecked";
 	public static final String SINGLE_INVITE_SMS_ALERT_CHECKED = "singleSmsAlertChecked";
 	public static final String FTUE_ADD_SMS_ALERT_CHECKED = "ftueSmsAlertChecked";
-	public static final String IS_GAMES_ITEM_CLICKED ="isGamesItemClicked";
-	public static final String IS_REWARDS_ITEM_CLICKED ="isRewardsItemClicked";
-	public static final String IS_OF_ICON_CLICKED ="isOfIconClicked";
-	
+	public static final String IS_GAMES_ITEM_CLICKED = "isGamesItemClicked";
+	public static final String IS_REWARDS_ITEM_CLICKED = "isRewardsItemClicked";
+	public static final String IS_OF_ICON_CLICKED = "isOfIconClicked";
+
 	public static final String DONE = "Done";
 	public static final String PIN_ERROR = "PinError";
 	public static final String ADDRESS_BOOK_ERROR = "AddressBookError";
