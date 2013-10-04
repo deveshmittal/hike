a35499dc7bbac176bf9eeafe0a0cc0ad726f5498
Rishabh Sharma <rs@bsb.in>
2013-10-04 18:09:41 +0530

    Fixed google bug in the compatibility package http://code.google.com/p/android/issues/detail?id=19917

--- src/com/bsb/hike/utils/HikeAppStateBaseFragmentActivity.java
+++ src/com/bsb/hike/utils/HikeAppStateBaseFragmentActivity.java
@@ -68,6 +68,14 @@ public class HikeAppStateBaseFragmentActivity extends SherlockFragmentActivity
 		}
 	}
 
+	protected void onSaveInstanceState(Bundle outState) {
+		// first saving my state, so the bundle wont be empty.
+		// http://code.google.com/p/android/issues/detail?id=19917
+		outState.putString("WORKAROUND_FOR_BUG_19917_KEY",
+				"WORKAROUND_FOR_BUG_19917_VALUE");
+		super.onSaveInstanceState(outState);
+	}
+
 	@Override
 	protected void onStop() {
 		Log.d(TAG + getClass().getSimpleName(), "OnStop");
