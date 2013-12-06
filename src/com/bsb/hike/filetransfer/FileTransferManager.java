2201b5ae578848daba1b6f85e01009d9a74c7dc6
gaurav-m <gauravm@bsb.in>
2013-12-06 16:09:41 +0530

    enum network type name change

--- src/com/bsb/hike/filetransfer/FileTransferManager.java
+++ src/com/bsb/hike/filetransfer/FileTransferManager.java
@@ -92,7 +92,7 @@ public class FileTransferManager
 				return 64 * 1024;
 			}
 		},
-		G4
+		FOUR_G
 		{
 			@Override
 			public int getMaxChunkSize()
@@ -106,7 +106,7 @@ public class FileTransferManager
 				return 16 * 1024;
 			}
 		},
-		G3
+		THREE_G
 		{
 			@Override
 			public int getMaxChunkSize()
@@ -120,7 +120,7 @@ public class FileTransferManager
 				return 16 * 1024;
 			}
 		},
-		G2
+		TWO_2
 		{
 			@Override
 			public int getMaxChunkSize()
@@ -524,14 +524,14 @@ public class FileTransferManager
 		case TelephonyManager.NETWORK_TYPE_LTE:		// ~ 10+ Mbps	// API level 11
 		case TelephonyManager.NETWORK_TYPE_HSPAP:	// ~ 10-20 Mbps	// API level 13
 		case TelephonyManager.NETWORK_TYPE_EVDO_B:	// ~ 5 Mbps		// API level 9
-			return NetworkType.G4;					//return 512 * 1024;
+			return NetworkType.FOUR_G;					//return 512 * 1024;
 		case TelephonyManager.NETWORK_TYPE_EVDO_0:	// ~ 400-1000 kbps
 		case TelephonyManager.NETWORK_TYPE_EVDO_A:	// ~ 600-1400 kbps
 		case TelephonyManager.NETWORK_TYPE_HSDPA:	// ~ 2-14 Mbps
 		case TelephonyManager.NETWORK_TYPE_HSPA:	// ~ 700-1700 kbps
 		case TelephonyManager.NETWORK_TYPE_UMTS:	// ~ 400-7000 kbps 
 		case TelephonyManager.NETWORK_TYPE_EHRPD:	// ~ 1-2 Mbps	// API level 11
-			return NetworkType.G3;					//return 256 *1024;
+			return NetworkType.THREE_G;					//return 256 *1024;
 		case TelephonyManager.NETWORK_TYPE_1xRTT:	// ~ 50-100 kbps
 		case TelephonyManager.NETWORK_TYPE_CDMA:	// ~ 14-64 kbps
 		case TelephonyManager.NETWORK_TYPE_EDGE:	// ~ 50-100 kbps
@@ -539,7 +539,7 @@ public class FileTransferManager
 		case TelephonyManager.NETWORK_TYPE_IDEN:	// ~25 kbps		// API level 8
 		case TelephonyManager.NETWORK_TYPE_UNKNOWN:
 		default:
-			return NetworkType.G2;					//return 24 * 1024;
+			return NetworkType.TWO_2;					//return 24 * 1024;
 		}
 	}
 	
