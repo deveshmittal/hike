1a10abd43be6fbb436c786df439ed9c78a78b5d7
Rishabh Sharma <rs@bsb.in>
2014-01-08 23:18:50 +0530

    Fix for a crash seen on the dev console. This can be the only reason for getting an NPE in the method

--- src/com/bsb/hike/utils/ContactUtils.java
+++ src/com/bsb/hike/utils/ContactUtils.java
@@ -219,6 +219,14 @@ public class ContactUtils {
 			phones = ctx.getContentResolver().query(Phone.CONTENT_URI,
 					new String[] { Phone.CONTACT_ID, Phone.NUMBER }, null,
 					null, null);
+			/*
+			 * Added this check for an issue where the cursor is null in some
+			 * random cases (We suspect that happens when hotmail contacts are
+			 * synced.)
+			 */
+			if (phones == null) {
+				return null;
+			}
 
 			int numberColIdx = phones.getColumnIndex(Phone.NUMBER);
 			int idColIdx = phones.getColumnIndex(Phone.CONTACT_ID);
