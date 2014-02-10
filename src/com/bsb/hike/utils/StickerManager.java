0390b85ee3d7a14381e4906534368b56ebe391be
vermapankaj <pankajv@bsb.in>
2014-02-10 21:55:27 +0530

    fixed the bug where sticker category was not removing on sending remove category packet

--- src/com/bsb/hike/utils/StickerManager.java
+++ src/com/bsb/hike/utils/StickerManager.java
@@ -447,7 +447,7 @@ public class StickerManager
 		while (it.hasNext())
 		{
 			StickerCategory cat = it.next();
-			if (cat.categoryId.equals(removedCategoryId))
+			if (cat.categoryId.name().equals(removedCategoryId))
 			{
 				removeCategoryFromRecents(cat);
 				it.remove();
