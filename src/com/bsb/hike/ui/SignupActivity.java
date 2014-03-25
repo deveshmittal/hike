de1d2cb98ee4719202d43b468018fd4bab40904c
vermapankaj <pankajv@bsb.in>
2014-03-25 15:53:22 +0530

    started pulling number automatically when available in signup

--- src/com/bsb/hike/ui/SignupActivity.java
+++ src/com/bsb/hike/ui/SignupActivity.java
@@ -695,7 +695,12 @@ public class SignupActivity extends HikeAppStateBaseFragmentActivity implements 
 		countryPicker.setEnabled(true);
 		
 		setupCountryCodeData();
-		
+		TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+		String number = manager.getLine1Number();
+		if(number !=null)
+		{
+			enterEditText.setText(number);
+		}
 		infoTxt.setText(msisdnErrorDuringSignup ? R.string.enter_phone_again_signup : R.string.whats_your_number);
 		invalidNum.setVisibility(View.INVISIBLE);
 		loadingText.setText(R.string.hang_on);
