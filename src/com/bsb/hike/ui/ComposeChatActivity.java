1e45f82193d56fd1261d4c48465af40c2e8d7069
Rishabh Sharma <rs@bsb.in>
2014-04-08 15:57:32 +0530

    Removed unused import

--- src/com/bsb/hike/ui/ComposeChatActivity.java
+++ src/com/bsb/hike/ui/ComposeChatActivity.java
@@ -50,7 +50,6 @@ import com.bsb.hike.utils.StickerManager;
 import com.bsb.hike.utils.Utils;
 import com.bsb.hike.view.TagEditText;
 import com.bsb.hike.view.TagEditText.TagEditorListener;
-import com.google.android.gms.internal.co;
 
 public class ComposeChatActivity extends HikeAppStateBaseFragmentActivity implements TagEditorListener, OnItemClickListener, HikePubSub.Listener
 {
