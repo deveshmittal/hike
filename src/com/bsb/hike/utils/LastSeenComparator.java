a722c231ddac7560c15e0859b3222dc37744a8d2
GK <gk@bsb.in>
2014-04-09 20:22:59 +0530

    Fixed bug#27888 'Crash'

--- src/com/bsb/hike/utils/LastSeenComparator.java
+++ src/com/bsb/hike/utils/LastSeenComparator.java
@@ -16,66 +16,37 @@ public class LastSeenComparator implements Comparator<ContactInfo>
 		FavoriteType lhsFavoriteType = lhs.getFavoriteType();
 		FavoriteType rhsFavoriteType = rhs.getFavoriteType();
 
-		if (lhsFavoriteType != rhsFavoriteType)
+		// if both the values are FRIEND REQUEST then compare on the basis of contacts
+		if (lhsFavoriteType == FavoriteType.REQUEST_RECEIVED && rhsFavoriteType == FavoriteType.REQUEST_RECEIVED)
+			return lhs.compareTo(rhs);
+
+		else if (lhsFavoriteType == FavoriteType.REQUEST_RECEIVED)
+			return -1;
+		
+		else if (rhsFavoriteType == FavoriteType.REQUEST_RECEIVED)
+			return 1;
+
+		else if (lastSeenPref)
 		{
-			if (lhsFavoriteType == FavoriteType.REQUEST_RECEIVED)
-			{
+			boolean l = hasLastSeenValue(lhs);
+			boolean r = hasLastSeenValue(rhs);
+			if(l && r) // both have last seen values and are online
+				return lhs.compareTo(rhs);
+			
+			else if(l) // only left has last seen value
 				return -1;
-			}
-			else if (rhsFavoriteType == FavoriteType.REQUEST_RECEIVED)
-			{
+			
+			else if(r) // only right has last seen value
 				return 1;
-			}
+			
+			else // neither left nor right has last seen value
+				lhs.compareTo(rhs);
 		}
-
-		if (lastSeenPref)
-		{
-			if (hasLastSeenValue(lhsFavoriteType))
-			{
-				if (!hasLastSeenValue(rhsFavoriteType))
-				{
-					if (lhs.getOffline() == 0)
-					{
-						return -1;
-					}
-				}
-				int value = compareOfflineValues(lhs.getOffline(), rhs.getOffline());
-				if (value != 0)
-				{
-					return value;
-				}
-			}
-			else if (hasLastSeenValue(rhsFavoriteType) && !hasLastSeenValue(lhsFavoriteType))
-			{
-				if (rhs.getOffline() == 0)
-				{
-					return 1;
-				}
-			}
-		}
-
 		return lhs.compareTo(rhs);
 	}
 
-	private boolean hasLastSeenValue(FavoriteType favoriteType)
+	private boolean hasLastSeenValue(ContactInfo lhs)
 	{
-		return favoriteType == FavoriteType.FRIEND || favoriteType == FavoriteType.REQUEST_RECEIVED_REJECTED;
+		return (lhs.getFavoriteType() == FavoriteType.FRIEND || lhs.getFavoriteType() == FavoriteType.REQUEST_RECEIVED_REJECTED) && (lhs.getOffline() == 0);
 	}
-
-	private int compareOfflineValues(int lhs, int rhs)
-	{
-		if (lhs != rhs)
-		{
-			if (lhs == 0)
-			{
-				return -1;
-			}
-			else if (rhs == 0)
-			{
-				return 1;
-			}
-		}
-		return 0;
-	}
-
 }
