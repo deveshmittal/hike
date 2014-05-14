9ff91de18d0ac1550e15c1d97fafbcc1d41007f6
gaurav-m <gauravm@bsb.in>
2014-05-14 16:01:00 +0530

    fixed issue when receiver offline message is shown just after upload

--- src/com/bsb/hike/filetransfer/UploadFileTask.java
+++ src/com/bsb/hike/filetransfer/UploadFileTask.java
@@ -963,6 +963,7 @@ public class UploadFileTask extends FileTransferBase
 		}
 		catch (Exception e)
 		{
+			e.printStackTrace();
 			Logger.e(getClass().getSimpleName(), "FT Upload error : " + e.getMessage());
 			if (retryAttempts >= MAX_RETRY_ATTEMPTS || e.getMessage() == null)
 			{
@@ -1075,10 +1076,10 @@ public class UploadFileTask extends FileTransferBase
 			FileTransferManager.getInstance(context).removeTask(((ConvMessage) userContext).getMsgID());
 			LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(HikePubSub.FILE_TRANSFER_PROGRESS_UPDATED));
 		}
-//		if (result == FTResult.SUCCESS)
-//		{
-//			((ConvMessage) userContext).setTimestamp(System.currentTimeMillis() / 1000);
-//		}
+		if (result == FTResult.SUCCESS)
+		{
+			((ConvMessage) userContext).setTimestamp(System.currentTimeMillis() / 1000);
+		}
 		else if (result != FTResult.PAUSED)
 		{
 			final int errorStringId = result == FTResult.READ_FAIL ? R.string.unable_to_read : result == FTResult.CANCELLED ? R.string.upload_cancelled : result == FTResult.FAILED_UNRECOVERABLE ? R.string.upload_failed
