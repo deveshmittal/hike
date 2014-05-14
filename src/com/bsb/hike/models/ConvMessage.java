9fb4f2bedddfa5486c1e349f83411d87a13f5549
gauravKhanna <gaurav@bsb.in>
2014-05-14 20:20:39 +0530

    added property to make it blockadd header for unknown contacts

--- src/com/bsb/hike/models/ConvMessage.java
+++ src/com/bsb/hike/models/ConvMessage.java
@@ -21,6 +21,8 @@ import com.bsb.hike.utils.Utils;
 
 public class ConvMessage
 {
+	private boolean isBlockAddHeader;
+
 	private long msgID; // this corresponds to msgID stored in sender's DB
 
 	private long mappedMsgId; // this corresponds to msgID stored in receiver's
@@ -800,5 +802,15 @@ public class ConvMessage
 	public void setShouldShowPush(boolean shouldShowPush)
 	{
 		this.shouldShowPush = shouldShowPush;
+	}
+
+	public void setBlockAddHeader(boolean isBlockAddHeader)
+	{
+		this.isBlockAddHeader = isBlockAddHeader;
+	}
+
+	public boolean isBlockAddHeader()
+	{
+		return isBlockAddHeader;
 	}
 }
