4e2961c8fe6b7c0294b877fbc53dc98997483874
Rishabh Sharma <rs@bsb.in>
2014-05-28 13:39:28 +0530

    Moved the logic of getting the conversation's last message text to a separate method

--- src/com/bsb/hike/adapters/ConversationsAdapter.java
+++ src/com/bsb/hike/adapters/ConversationsAdapter.java
@@ -302,154 +302,8 @@ public class ConversationsAdapter extends ArrayAdapter<Conversation>
 
 			TextView messageView = (TextView) v.findViewById(R.id.last_message);
 
-			MessageMetadata metadata = message.getMetadata();
-
-			CharSequence markedUp = null;
-			if (message.isFileTransferMessage())
-			{
-				markedUp = HikeFileType.getFileTypeMessage(context, metadata.getHikeFiles().get(0).getHikeFileType(), message.isSent());
-				if ((conversation instanceof GroupConversation) && !message.isSent())
-				{
-					markedUp = Utils.addContactName(((GroupConversation) conversation).getGroupParticipantFirstName(message.getGroupParticipantMsisdn()), markedUp);
-				}
-			}
-			else if (message.getParticipantInfoState() == ParticipantInfoState.PARTICIPANT_JOINED)
-			{
-				JSONArray participantInfoArray = metadata.getGcjParticipantInfo();
-
-				String highlight = Utils.getGroupJoinHighlightText(participantInfoArray, (GroupConversation) conversation);
-
-				if (metadata.isNewGroup())
-				{
-					markedUp = String.format(context.getString(R.string.new_group_message), highlight);
-				}
-				else
-				{
-					markedUp = String.format(context.getString(R.string.add_to_group_message), highlight);
-				}
-			}
-			else if (message.getParticipantInfoState() == ParticipantInfoState.DND_USER)
-			{
-				JSONArray dndNumbers = metadata.getDndNumbers();
-				if (dndNumbers != null && dndNumbers.length() > 0)
-				{
-					StringBuilder dndNames = new StringBuilder();
-					for (int i = 0; i < dndNumbers.length(); i++)
-					{
-						String dndName;
-						dndName = conversation instanceof GroupConversation ? ((GroupConversation) conversation).getGroupParticipantFirstName(dndNumbers.optString(i)) : Utils
-								.getFirstName(conversation.getLabel());
-						if (i < dndNumbers.length() - 2)
-						{
-							dndNames.append(dndName + ", ");
-						}
-						else if (i < dndNumbers.length() - 1)
-						{
-							dndNames.append(dndName + " and ");
-						}
-						else
-						{
-							dndNames.append(dndName);
-						}
-					}
-					markedUp = String.format(context.getString(conversation instanceof GroupConversation ? R.string.dnd_msg_gc : R.string.dnd_one_to_one), dndNames.toString());
-				}
-			}
-			else if (message.getParticipantInfoState() == ParticipantInfoState.INTRO_MESSAGE)
-			{
-				if (conversation.isOnhike())
-				{
-					boolean firstIntro = conversation.getMsisdn().hashCode() % 2 == 0;
-					markedUp = String.format(context.getString(firstIntro ? R.string.start_thread1 : R.string.start_thread1), Utils.getFirstName(conversation.getLabel()));
-				}
-				else
-				{
-					markedUp = String.format(context.getString(R.string.intro_sms_thread), Utils.getFirstName(conversation.getLabel()));
-				}
-			}
-			else if (message.getParticipantInfoState() == ParticipantInfoState.USER_JOIN)
-			{
-				String participantName;
-				if (conversation instanceof GroupConversation)
-				{
-					String participantMsisdn = metadata.getMsisdn();
-					participantName = ((GroupConversation) conversation).getGroupParticipantFirstName(participantMsisdn);
-				}
-				else
-				{
-					participantName = Utils.getFirstName(conversation.getLabel());
-				}
-				markedUp = context.getString(metadata.isOldUser() ? R.string.user_back_on_hike : R.string.joined_hike_new, participantName);
-
-			}
-			else if (message.getParticipantInfoState() == ParticipantInfoState.PARTICIPANT_LEFT || message.getParticipantInfoState() == ParticipantInfoState.GROUP_END)
-			{
-
-				if (message.getParticipantInfoState() == ParticipantInfoState.PARTICIPANT_LEFT)
-				{
-					// Showing the block internation sms message if the user was
-					// booted because of that reason
-					String participantMsisdn = metadata.getMsisdn();
-					String participantName = ((GroupConversation) conversation).getGroupParticipantFirstName(participantMsisdn);
-					markedUp = String.format(context.getString(R.string.left_conversation), participantName);
-				}
-				else
-				{
-					markedUp = context.getString(R.string.group_chat_end);
-				}
-			}
-			else if (message.getParticipantInfoState() == ParticipantInfoState.CHANGED_GROUP_NAME)
-			{
-				String msisdn = metadata.getMsisdn();
-
-				String userMsisdn = context.getSharedPreferences(HikeMessengerApp.ACCOUNT_SETTINGS, 0).getString(HikeMessengerApp.MSISDN_SETTING, "");
-
-				String participantName = userMsisdn.equals(msisdn) ? context.getString(R.string.you) : ((GroupConversation) conversation).getGroupParticipantFirstName(msisdn);
-
-				markedUp = String.format(context.getString(R.string.change_group_name), participantName);
-			}
-			else if (message.getParticipantInfoState() == ParticipantInfoState.BLOCK_INTERNATIONAL_SMS)
-			{
-				markedUp = context.getString(R.string.block_internation_sms);
-			}
-			else if (message.getParticipantInfoState() == ParticipantInfoState.CHAT_BACKGROUND)
-			{
-				String msisdn = metadata.getMsisdn();
-				String userMsisdn = context.getSharedPreferences(HikeMessengerApp.ACCOUNT_SETTINGS, 0).getString(HikeMessengerApp.MSISDN_SETTING, "");
-
-				String nameString;
-				if (conversation instanceof GroupConversation)
-				{
-					nameString = userMsisdn.equals(msisdn) ? context.getString(R.string.you) : ((GroupConversation) conversation).getGroupParticipantFirstName(msisdn);
-				}
-				else
-				{
-					nameString = userMsisdn.equals(msisdn) ? context.getString(R.string.you) : Utils.getFirstName(conversation.getLabel());
-				}
-
-				markedUp = context.getString(R.string.chat_bg_changed, nameString);
-			}
-			else
-			{
-				String msg = message.getMessage();
-				/*
-				 * Making sure this string is never null.
-				 */
-				if (msg == null)
-				{
-					msg = "";
-				}
-				markedUp = msg.substring(0, Math.min(msg.length(), HikeConstants.MAX_MESSAGE_PREVIEW_LENGTH));
-				// For showing the name of the contact that sent the message in
-				// a group chat
-				if (conversation instanceof GroupConversation && !TextUtils.isEmpty(message.getGroupParticipantMsisdn())
-						&& message.getParticipantInfoState() == ParticipantInfoState.NO_INFO)
-				{
-					markedUp = Utils.addContactName(((GroupConversation) conversation).getGroupParticipantFirstName(message.getGroupParticipantMsisdn()), markedUp);
-				}
-				SmileyParser smileyParser = SmileyParser.getInstance();
-				markedUp = smileyParser.addSmileySpans(markedUp, true);
-			}
+			CharSequence markedUp = getConversationText(conversation, message);
+			
 			messageView.setVisibility(View.VISIBLE);
 			messageView.setText(markedUp);
 			TextView tsView = (TextView) v.findViewById(R.id.last_message_timestamp);
@@ -468,6 +322,161 @@ public class ConversationsAdapter extends ArrayAdapter<Conversation>
 		ImageView avatarView = (ImageView) v.findViewById(R.id.avatar);
 		iconLoader.loadImage(conversation.getMsisdn(), true, avatarView, true);
 		return v;
+	}
+
+	private CharSequence getConversationText(Conversation conversation, ConvMessage message)
+	{
+		Context context = getContext();
+		MessageMetadata metadata = message.getMetadata();
+		CharSequence markedUp = null;
+
+		if (message.isFileTransferMessage())
+		{
+			markedUp = HikeFileType.getFileTypeMessage(context, metadata.getHikeFiles().get(0).getHikeFileType(), message.isSent());
+			if ((conversation instanceof GroupConversation) && !message.isSent())
+			{
+				markedUp = Utils.addContactName(((GroupConversation) conversation).getGroupParticipantFirstName(message.getGroupParticipantMsisdn()), markedUp);
+			}
+		}
+		else if (message.getParticipantInfoState() == ParticipantInfoState.PARTICIPANT_JOINED)
+		{
+			JSONArray participantInfoArray = metadata.getGcjParticipantInfo();
+
+			String highlight = Utils.getGroupJoinHighlightText(participantInfoArray, (GroupConversation) conversation);
+
+			if (metadata.isNewGroup())
+			{
+				markedUp = String.format(context.getString(R.string.new_group_message), highlight);
+			}
+			else
+			{
+				markedUp = String.format(context.getString(R.string.add_to_group_message), highlight);
+			}
+		}
+		else if (message.getParticipantInfoState() == ParticipantInfoState.DND_USER)
+		{
+			JSONArray dndNumbers = metadata.getDndNumbers();
+			if (dndNumbers != null && dndNumbers.length() > 0)
+			{
+				StringBuilder dndNames = new StringBuilder();
+				for (int i = 0; i < dndNumbers.length(); i++)
+				{
+					String dndName;
+					dndName = conversation instanceof GroupConversation ? ((GroupConversation) conversation).getGroupParticipantFirstName(dndNumbers.optString(i)) : Utils
+							.getFirstName(conversation.getLabel());
+					if (i < dndNumbers.length() - 2)
+					{
+						dndNames.append(dndName + ", ");
+					}
+					else if (i < dndNumbers.length() - 1)
+					{
+						dndNames.append(dndName + " and ");
+					}
+					else
+					{
+						dndNames.append(dndName);
+					}
+				}
+				markedUp = String.format(context.getString(conversation instanceof GroupConversation ? R.string.dnd_msg_gc : R.string.dnd_one_to_one), dndNames.toString());
+			}
+		}
+		else if (message.getParticipantInfoState() == ParticipantInfoState.INTRO_MESSAGE)
+		{
+			if (conversation.isOnhike())
+			{
+				boolean firstIntro = conversation.getMsisdn().hashCode() % 2 == 0;
+				markedUp = String.format(context.getString(firstIntro ? R.string.start_thread1 : R.string.start_thread1), Utils.getFirstName(conversation.getLabel()));
+			}
+			else
+			{
+				markedUp = String.format(context.getString(R.string.intro_sms_thread), Utils.getFirstName(conversation.getLabel()));
+			}
+		}
+		else if (message.getParticipantInfoState() == ParticipantInfoState.USER_JOIN)
+		{
+			String participantName;
+			if (conversation instanceof GroupConversation)
+			{
+				String participantMsisdn = metadata.getMsisdn();
+				participantName = ((GroupConversation) conversation).getGroupParticipantFirstName(participantMsisdn);
+			}
+			else
+			{
+				participantName = Utils.getFirstName(conversation.getLabel());
+			}
+			markedUp = context.getString(metadata.isOldUser() ? R.string.user_back_on_hike : R.string.joined_hike_new, participantName);
+
+		}
+		else if (message.getParticipantInfoState() == ParticipantInfoState.PARTICIPANT_LEFT || message.getParticipantInfoState() == ParticipantInfoState.GROUP_END)
+		{
+
+			if (message.getParticipantInfoState() == ParticipantInfoState.PARTICIPANT_LEFT)
+			{
+				// Showing the block internation sms message if the user was
+				// booted because of that reason
+				String participantMsisdn = metadata.getMsisdn();
+				String participantName = ((GroupConversation) conversation).getGroupParticipantFirstName(participantMsisdn);
+				markedUp = String.format(context.getString(R.string.left_conversation), participantName);
+			}
+			else
+			{
+				markedUp = context.getString(R.string.group_chat_end);
+			}
+		}
+		else if (message.getParticipantInfoState() == ParticipantInfoState.CHANGED_GROUP_NAME)
+		{
+			String msisdn = metadata.getMsisdn();
+
+			String userMsisdn = context.getSharedPreferences(HikeMessengerApp.ACCOUNT_SETTINGS, 0).getString(HikeMessengerApp.MSISDN_SETTING, "");
+
+			String participantName = userMsisdn.equals(msisdn) ? context.getString(R.string.you) : ((GroupConversation) conversation).getGroupParticipantFirstName(msisdn);
+
+			markedUp = String.format(context.getString(R.string.change_group_name), participantName);
+		}
+		else if (message.getParticipantInfoState() == ParticipantInfoState.BLOCK_INTERNATIONAL_SMS)
+		{
+			markedUp = context.getString(R.string.block_internation_sms);
+		}
+		else if (message.getParticipantInfoState() == ParticipantInfoState.CHAT_BACKGROUND)
+		{
+			String msisdn = metadata.getMsisdn();
+			String userMsisdn = context.getSharedPreferences(HikeMessengerApp.ACCOUNT_SETTINGS, 0).getString(HikeMessengerApp.MSISDN_SETTING, "");
+
+			String nameString;
+			if (conversation instanceof GroupConversation)
+			{
+				nameString = userMsisdn.equals(msisdn) ? context.getString(R.string.you) : ((GroupConversation) conversation).getGroupParticipantFirstName(msisdn);
+			}
+			else
+			{
+				nameString = userMsisdn.equals(msisdn) ? context.getString(R.string.you) : Utils.getFirstName(conversation.getLabel());
+			}
+
+			markedUp = context.getString(R.string.chat_bg_changed, nameString);
+		}
+		else
+		{
+			String msg = message.getMessage();
+			/*
+			 * Making sure this string is never null.
+			 */
+			if (msg == null)
+			{
+				msg = "";
+			}
+			markedUp = msg.substring(0, Math.min(msg.length(), HikeConstants.MAX_MESSAGE_PREVIEW_LENGTH));
+			// For showing the name of the contact that sent the message in
+			// a group chat
+			if (conversation instanceof GroupConversation && !TextUtils.isEmpty(message.getGroupParticipantMsisdn())
+					&& message.getParticipantInfoState() == ParticipantInfoState.NO_INFO)
+			{
+				markedUp = Utils.addContactName(((GroupConversation) conversation).getGroupParticipantFirstName(message.getGroupParticipantMsisdn()), markedUp);
+			}
+			SmileyParser smileyParser = SmileyParser.getInstance();
+			markedUp = smileyParser.addSmileySpans(markedUp, true);
+		}
+
+		return markedUp;
 	}
 
 	private class CountDownSetter extends CountDownTimer
