86f487a0ba69494de104fada29d22383ed1e8212
Rishabh Sharma <rs@bsb.in>
2014-06-10 19:17:32 +0530

    Fixed bug where we did not show the correct on hike value for unknown contacts in the group info

--- src/com/bsb/hike/db/HikeConversationsDatabase.java
+++ src/com/bsb/hike/db/HikeConversationsDatabase.java
@@ -1445,7 +1445,7 @@ public class HikeConversationsDatabase extends SQLiteOpenHelper
 				String groupId = c.getString(groupIdIdx);
 				String msisdn = c.getString(msisdnIdx);
 				msisdnSB.append(DatabaseUtils.sqlEscapeString(msisdn) + ",");
-				GroupParticipant groupParticipant = new GroupParticipant(new ContactInfo(msisdn, msisdn, c.getString(nameIdx), msisdn), c.getInt(hasLeftIdx) != 0,
+				GroupParticipant groupParticipant = new GroupParticipant(new ContactInfo(msisdn, msisdn, c.getString(nameIdx), msisdn, c.getInt(onHikeIdx) != 0), c.getInt(hasLeftIdx) != 0,
 						c.getInt(onDndIdx) != 0);
 
 				Map<String, GroupParticipant> participantList = groupIdParticipantsMap.get(groupId);
@@ -1895,7 +1895,8 @@ public class HikeConversationsDatabase extends SQLiteOpenHelper
 			{
 				String msisdn = c.getString(c.getColumnIndex(DBConstants.MSISDN));
 				allMsisdns.append(DatabaseUtils.sqlEscapeString(msisdn) + ",");
-				GroupParticipant groupParticipant = new GroupParticipant(new ContactInfo(msisdn, msisdn, c.getString(c.getColumnIndex(DBConstants.NAME)), msisdn), c.getInt(c
+				GroupParticipant groupParticipant = new GroupParticipant(new ContactInfo(msisdn, msisdn, c.getString(c.getColumnIndex(DBConstants.NAME)), msisdn, c.getInt(c
+						.getColumnIndex(DBConstants.ONHIKE)) != 0), c.getInt(c
 						.getColumnIndex(DBConstants.HAS_LEFT)) != 0, c.getInt(c.getColumnIndex(DBConstants.ON_DND)) != 0);
 				participantList.put(msisdn, groupParticipant);
 			}
