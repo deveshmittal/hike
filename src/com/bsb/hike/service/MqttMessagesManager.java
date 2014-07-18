811112555ccecc276c3215564706951654e8f4b6
anubhav7nov <anubhav.7nov@gmail.com>
2014-07-18 13:21:06 +0530

    fixed null pointer exception

--- src/com/bsb/hike/service/MqttMessagesManager.java
+++ src/com/bsb/hike/service/MqttMessagesManager.java
@@ -540,16 +540,16 @@ public class MqttMessagesManager
 		
 		if (convMessage.isGroupChat() && convMessage.getParticipantInfoState() == ParticipantInfoState.NO_INFO)
 		{
-			convMessage = convDb.showParticipantStatusMessage(convMessage.getMsisdn());
-			if (convMessage != null)
+			ConvMessage convMessageNew = convDb.showParticipantStatusMessage(convMessage.getMsisdn());
+			if (convMessageNew != null)
 			{
 				if(isBulkMessage)
 				{
-					addToLists(convMessage.getMsisdn(), convMessage);
+					addToLists(convMessageNew.getMsisdn(), convMessageNew);
 				}
 				else
 				{
-					this.pubSub.publish(HikePubSub.MESSAGE_RECEIVED, convMessage);
+					this.pubSub.publish(HikePubSub.MESSAGE_RECEIVED, convMessageNew);
 				}
 			}
 		}
