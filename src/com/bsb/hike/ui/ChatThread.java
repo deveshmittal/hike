e80d2b9093558c48aa57ab5ac0dcf0a5ee488d75
gauravKhanna <gaurav@bsb.in>
2014-07-28 18:47:41 +0530

    solved bug , need to hide pin view on new intent

--- src/com/bsb/hike/ui/ChatThread.java
+++ src/com/bsb/hike/ui/ChatThread.java
@@ -2003,6 +2003,9 @@ public class ChatThread extends HikeAppStateBaseFragmentActivity implements Hike
 			Logger.w("ChatThread", "DIFFERENT MSISDN CLOSING CONTEXT MENU!!");
 			closeContextMenu();
 		}
+		if(showingImpMessagePin){
+			dismissPinCreateView();
+		}
 	}
 
 	private void initiateFileTransferFromIntentData(String fileType, String filePath)
