d1b37a7a6ab4b18470c1a9e835008eaa1d887f57
piyushghai <piyush@hike.in>
2014-08-21 17:39:47 +0530

    Moved Async loading events from SharedMediaAdapter to PhotoViewerFragment

--- src/com/bsb/hike/adapters/SharedMediaAdapter.java
+++ src/com/bsb/hike/adapters/SharedMediaAdapter.java
@@ -1,13 +1,10 @@
 package com.bsb.hike.adapters;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -17,13 +14,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 
-import com.bsb.hike.HikeConstants;
 import com.bsb.hike.R;
-import com.bsb.hike.db.HikeConversationsDatabase;
 import com.bsb.hike.models.HikeSharedFile;
 import com.bsb.hike.smartImageLoader.SharedFileImageLoader;
 import com.bsb.hike.utils.Logger;
-import com.bsb.hike.utils.Utils;
 
 public class SharedMediaAdapter extends PagerAdapter
 {
@@ -33,26 +27,8 @@ public class SharedMediaAdapter extends PagerAdapter
 
 	private ArrayList<HikeSharedFile> sharedMediaItems;
 
-	private boolean reachedEndLeft = false;
-	
-	private boolean reachedEndRight = false;
-	
-	private boolean loadingMoreItems = false;
-
 	private Context context;
 
-	private String msisdn;
-	
-	private long minMsgId;
-	
-	private long maxMsgId;
-	
-	private boolean applyOffset = false;
-	
-	private ViewPager selectedPager;
-	
-	private int prevPosition = 0;
-	
 	private String TAG = "SharedMediaAdapter";
 	
 	private String IMAGE_TAG = "image";
@@ -63,10 +39,6 @@ public class SharedMediaAdapter extends PagerAdapter
 		this.layoutInflater = LayoutInflater.from(this.context);
 		this.sharedMediaLoader = new SharedFileImageLoader(context, size_image);
 		this.sharedMediaItems = sharedMediaItems;
-		this.msisdn = msisdn;
-		this.minMsgId = sharedMediaItems.get(0).getMsgId();
-		this.maxMsgId = sharedMediaItems.get(sharedMediaItems.size()-1).getMsgId();
-		this.selectedPager = viewPager;
 	}
 
 	@Override
@@ -124,119 +96,9 @@ public class SharedMediaAdapter extends PagerAdapter
 
 		galleryImageView.setScaleType(ScaleType.FIT_CENTER);
 		sharedMediaLoader.loadImage(sharedMediaItem.getFilePath(), galleryImageView, false);
-		((ViewPager) container).addView(page);
-		Logger.d(TAG, "Position : " + position + " : " + getCount() + " : " + loadingMoreItems + " Right End ? : " + reachedEndRight + "Left End ? : " + reachedEndLeft);
 		
-		if (!reachedEndRight && !loadingMoreItems && position == (getCount() - 3))
-		{
-			loadingMoreItems = true;
-			Logger.d(TAG,"loading items from right : " + maxMsgId);
-			loadItems(reachedEndRight, maxMsgId, HikeConstants.MAX_MEDIA_ITEMS_TO_LOAD_INITIALLY, true);
-		}
-
-		if (!reachedEndLeft && !loadingMoreItems && position == 3)
-		{
-			loadingMoreItems = true;
-			Logger.d(TAG, "loading items from left : " + minMsgId);
-			loadItems(reachedEndLeft, minMsgId, HikeConstants.MAX_MEDIA_ITEMS_TO_LOAD_INITIALLY, false, true, position);
-
-		}
+		((ViewPager) container).addView(page);
 		return page;
 	}
 
-	public void loadItems(boolean reachedEnd, long maxMsgId, int limit, boolean itemsToRight)
-	{
-		if (Utils.isHoneycombOrHigher())
-		{
-			new GetMoreItemsTask(reachedEnd, maxMsgId, limit, itemsToRight).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-		}
-		else
-		{
-			 new GetMoreItemsTask(reachedEnd, maxMsgId, limit, itemsToRight).execute();
-		}
-	}
-	//function called to load items to the left of viewpager
-	public void loadItems(boolean reachedEnd, long msgId, int limit, boolean itemsToRight, boolean applyOffset, int prevPosition)
-	{
-		this.applyOffset = applyOffset;
-		this.prevPosition = prevPosition;
-		if (Utils.isHoneycombOrHigher())
-		{
-			new GetMoreItemsTask(reachedEnd, msgId, limit, itemsToRight).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-		}
-		else
-		{
-			 new GetMoreItemsTask(reachedEnd, msgId, limit, itemsToRight).execute();
-		}
-	}
-	/*
-	 * AsyncTask to load more media in the background thread
-	 */
-	public class GetMoreItemsTask extends AsyncTask<Void, Void, List<HikeSharedFile>>
-	{
-		private boolean itemsToRight;
-
-		private Long msgId;
-
-		private int limit;
-
-		public GetMoreItemsTask(boolean reachEnd, long msgId, int limit, boolean itemsToRight)
-		{
-			this.itemsToRight = itemsToRight;
-			this.msgId = msgId;
-			this.limit = limit;
-		}
-
-		@Override
-		protected List<HikeSharedFile> doInBackground(Void... params)
-		{
-			return HikeConversationsDatabase.getInstance().getSharedMedia(msisdn, limit, msgId, true, itemsToRight);
-		}
-
-		@Override
-		protected void onPostExecute(List<HikeSharedFile> result)
-		{
-			if (!result.isEmpty())
-			{  
-				if (itemsToRight)    //Loading items to the right of the viewpager
-				{
-					sharedMediaItems.addAll(sharedMediaItems.size() , result);
-				}
-				else				//Loading them to the left
-				{	Collections.reverse(result);
-					sharedMediaItems.addAll(0, result);
-					
-				}
-			   //Recalculating the min and Max msgIds, for further loading
-				notifyDataSetChanged();
-				minMsgId = sharedMediaItems.get(0).getMsgId();
-				maxMsgId = sharedMediaItems.get(sharedMediaItems.size()-1).getMsgId();
-
-				if(applyOffset && !itemsToRight)   //Offset needed to correctly display current item in viewpager, if more items are loaded to left
-				{
-					selectedPager.setCurrentItem(result.size() + prevPosition , false); 
-					applyOffset = false;
-				}
-			}
-			
-			else
-			{
-				/*
-				 * This signifies that we've reached the end. No need to query the db anymore unless we find a new media item.
-				 */
-				if (itemsToRight)
-				{
-					reachedEndRight = true;
-					Logger.d(TAG, "Reached right end");
-				}
-				else
-				{
-					reachedEndLeft = true;
-					Logger.d(TAG, "Reached left end");
-				}
-			}
-			
-			loadingMoreItems = false;
-		}
-	}
 }
