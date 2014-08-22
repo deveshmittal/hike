49369b4599eefdcc8d5154b4ea42335ab4035745
piyushghai <piyush@hike.in>
2014-08-22 17:06:45 +0530

    Added request window feature for action bar overlay in gallery activity

--- src/com/bsb/hike/ui/GalleryActivity.java
+++ src/com/bsb/hike/ui/GalleryActivity.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -64,6 +65,7 @@ public class GalleryActivity extends HikeAppStateBaseFragmentActivity implements
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
 		setContentView(R.layout.gallery);
 
 		selectedGalleryItems = new HashMap<Long, GalleryItem>();
