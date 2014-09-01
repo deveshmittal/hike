1d0c27ad5644c513aa7751323e099f4383a0bcd1
Atul Mittal <atul@hike.in>
2014-09-01 16:22:59 +0530

    fixed incorrect unread message count issue #37200

--- src/com/bsb/hike/db/HikeConversationsDatabase.java
+++ src/com/bsb/hike/db/HikeConversationsDatabase.java
@@ -5031,7 +5031,8 @@ public class HikeConversationsDatabase extends SQLiteOpenHelper
 			}
 		}
 
+		unreadMessages += Utils.getNotificationCount(mContext.getSharedPreferences(HikeMessengerApp.ACCOUNT_SETTINGS, 0), true);
+
 		return unreadMessages;
 	}
 }
-
