11f249b2491b2c22e5bbaec958676ad01bf18528
Pankaj Verma <pankajv@bsb.in>
2014-09-04 16:40:29 +0530

    Fixed bug we were not catching ActivityNotFound exception in video file opening

--- src/com/bsb/hike/adapters/SharedMediaAdapter.java
+++ src/com/bsb/hike/adapters/SharedMediaAdapter.java
@@ -126,9 +126,7 @@ public class SharedMediaAdapter extends PagerAdapter implements OnClickListener
 			photoViewerFragment.toggleViewsVisibility();
 			break;
 		case VIDEO:
-			Intent openFile = new Intent(Intent.ACTION_VIEW);
-			openFile.setDataAndType(Uri.fromFile(sharedMediaItem.getFile()), sharedMediaItem.getFileTypeString());
-			context.startActivity(openFile);
+			HikeSharedFile.openFile(sharedMediaItem, context);
 			break;
 		default:
 			break;
