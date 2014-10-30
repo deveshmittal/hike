d9126f03572199df467f57ea92ed30f421773633
Suyash <suyash@Suyash.local>
2014-10-30 12:12:07 +0530

    Improved performance by decreasing network latency (Disabled use of Nagle's algorithm)

--- src/com/bsb/hike/filetransfer/UploadFileTask.java
+++ src/com/bsb/hike/filetransfer/UploadFileTask.java
@@ -1089,6 +1089,7 @@ public class UploadFileTask extends FileTransferBase
 		client.getParams().setParameter(CoreConnectionPNames.SOCKET_BUFFER_SIZE, bufferSize);
 		client.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 30 * 1000);
 		// client.getParams().setParameter(CoreConnectionPNames.STALE_CONNECTION_CHECK, false);
+		client.getParams().setParameter(CoreConnectionPNames.TCP_NODELAY, true);
 		long time = System.currentTimeMillis();
 		HttpPost post = new HttpPost(mUrl.toString());
 		String res = null;
