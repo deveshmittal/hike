402034aca0f17d70a3289db4d0c300000f91b983
gauravKhanna <gaurav@bsb.in>
2014-10-31 21:03:05 +0530

    Hike Platform constants added

---
+++ src/com/bsb/hike/platform/HikePlatformConstants.java
@@ -0,0 +1,22 @@
+package com.bsb.hike.platform;
+
+public interface HikePlatformConstants {
+	String DATA = "d";
+	String ASSETS = "assets";
+	String TEXTS = "textLinks";
+	String IMAGES = "images";
+	String VIDEOS = "videos";
+	String AUDIO = "audios";
+	String ACTIONS = "actions";
+	String CONTENT_TYPE = "contentType";
+	String TAG = "tag";
+	String TEXT = "title";
+	String BASE64_STRING = "thumbnail";
+	String URL ="url";
+	String MEDIA_SIZE = "size";
+	String DURATION = "duration";
+	String LAYOUT_ID = "layoutID";
+	String LOVE_ID = "loveID";
+	String RECEPIENT = "to";
+	String MESSAGE_TYPE = "t";
+}
