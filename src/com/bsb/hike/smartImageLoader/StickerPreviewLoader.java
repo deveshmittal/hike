ae1a452f4f5c2fddb30fd36d953b3a478ceb4745
anubhav7nov <anubhav.7nov@gmail.com>
2014-11-04 12:58:38 +0530

    added new stickerpreviewloader for stickershop and sticker settings screens

---
+++ src/com/bsb/hike/smartImageLoader/StickerPreviewLoader.java
@@ -0,0 +1,39 @@
+package com.bsb.hike.smartImageLoader;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.bsb.hike.utils.StickerManager;
+
+public class StickerPreviewLoader extends ImageWorker
+{
+	private Context ctx;
+	private boolean downloadIfNotFound;
+	
+	/**
+	 * 
+	 * @param ctx
+	 * @param downloadIfNotFound -- true if preview image should be downloaded if not found
+	 */
+	public StickerPreviewLoader(Context ctx, boolean downloadIfNotFound)
+	{
+		super();
+		this.ctx = ctx;
+		this.downloadIfNotFound = downloadIfNotFound;
+	}
+
+	@Override
+	protected Bitmap processBitmap(String categoryId)
+	{	
+		Bitmap bmp = StickerManager.getInstance().getCategoryPreviewAsset(ctx, categoryId, downloadIfNotFound);
+		return bmp;
+	}
+
+	@Override
+	protected Bitmap processBitmapOnUiThread(String data)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
