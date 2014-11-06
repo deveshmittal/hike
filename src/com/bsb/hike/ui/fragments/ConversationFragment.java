6cebe9e075d206291b07dea0608fd5655893cf6a
deepanshud <deepanshu@hike.in>
2014-11-06 14:05:53 +0530

    Showing footer only if bot is the only conversation

--- src/com/bsb/hike/ui/fragments/ConversationFragment.java
+++ src/com/bsb/hike/ui/fragments/ConversationFragment.java
@@ -408,7 +408,7 @@ public class ConversationFragment extends SherlockListFragment implements OnItem
 			editor.commit();
 		}
 
-		if(HikeConstants.FTUE_HIKE_SUPPORT.equals(conv.getMsisdn()) && HikeSharedPreferenceUtil.getInstance(getActivity()).getData(HikeConstants.SHOW_NUX_INVITE_MODE, false))
+		if(HikeConstants.FTUE_HIKE_SUPPORT.equals(conv.getMsisdn()) && HikeSharedPreferenceUtil.getInstance(getActivity()).getData(HikeConstants.SHOW_NUX_INVITE_MODE, false) && (displayedConversations.size() == 1))
 		{
 			HikeSharedPreferenceUtil.getInstance(getActivity()).saveData(HikeConstants.NUX_HOME_INVITE_FOOTER, true);
 		}
