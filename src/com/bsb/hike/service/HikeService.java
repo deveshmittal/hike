ad41e641862ca26f73e8d1d5a45116bbd361dfa8
gaurav-m <gauravm@bsb.in>
2014-12-08 18:21:44 +0530

    no need to specify the time, its default anyway

--- src/com/bsb/hike/service/HikeService.java
+++ src/com/bsb/hike/service/HikeService.java
@@ -722,9 +722,6 @@ public class HikeService extends Service
 		Calendar c = Calendar.getInstance();
 		c.add(Calendar.DAY_OF_MONTH, 1);
 		c.set(Calendar.HOUR_OF_DAY, 3);
-		c.set(Calendar.MINUTE, 0);
-		c.set(Calendar.SECOND, 0);
-		c.set(Calendar.MILLISECOND, 0);
 		scheduleTime = (c.getTimeInMillis() - System.currentTimeMillis());
 		postRunnableWithDelay(BackupAccountRunnable, scheduleTime);
 	}
