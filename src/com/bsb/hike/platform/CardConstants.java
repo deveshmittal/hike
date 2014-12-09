1e6f720d65e3e8606348575f831ff03be6b09556
shobhitm <shobhit.m@hoppr.com>
2014-12-09 20:03:39 +0530

    Constants added

--- src/com/bsb/hike/platform/CardConstants.java
+++ src/com/bsb/hike/platform/CardConstants.java
@@ -10,5 +10,8 @@ public class CardConstants {
     public static final int ARTICLE_CARD_LAYOUT = 4;
     public static final int COLOR_CARD_LAYOUT = 5;
     public static final int CHAT_THREAD_CARD_COUNT_TYPE_COUNT = 10;
+    public static final String PLAY_STORE_TEXT = "https://play.google.com/store/apps/details?id=";
+    public static final String CARD_NAME = "card_name";
+    public static final String ACTION_TEXT = "action_text";:q
 
 }
