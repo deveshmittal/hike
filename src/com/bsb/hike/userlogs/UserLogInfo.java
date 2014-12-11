f745ff91575d4770a3270373c0e04c316fab5f76
Umangjeet Singh Pahwa <umangjeet@hike.in>
2014-12-11 18:04:33 +0530

    adding timestamp as Location field, null checks in place & renaming appropriately

--- src/com/bsb/hike/userlogs/UserLogInfo.java
+++ src/com/bsb/hike/userlogs/UserLogInfo.java
@@ -13,6 +13,7 @@ import android.content.pm.PackageInfo;
 import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.net.Uri;
 import android.provider.CallLog;
 
@@ -60,16 +61,19 @@ public class UserLogInfo {
 	private static final String LATITUDE = "lat";
 	private static final String LONGITUDE = "long";
 	private static final String RADIUS = "rd";
+	private static final String TIMESTAMP = "ts";
 	
 	public static class LocLogPojo{
 		final double latitude;
 		final double longitude;
 		final float radius;
+		final long timeStamp;
 		
-		public LocLogPojo(double latitude, double longitude, float radius){
+		public LocLogPojo(double latitude, double longitude, float radius, long timeStamp){
 			this.latitude = latitude;
 			this.longitude = longitude;
 			this.radius = radius;
+			this.timeStamp = timeStamp;
 		}
 		
 		public JSONObject toJSON() throws JSONException{
@@ -77,6 +81,7 @@ public class UserLogInfo {
 			jsonObj.putOpt(LATITUDE, this.latitude);
 			jsonObj.putOpt(LONGITUDE,this.longitude);
 			jsonObj.putOpt(RADIUS, this.radius);
+			jsonObj.putOpt(TIMESTAMP, this.timeStamp);
 			return jsonObj;
 		}
 	}
@@ -160,11 +165,11 @@ public class UserLogInfo {
 	private static JSONObject getEncryptedJSON(Context ctx, JSONArray jsonLogArray, int flag) throws JSONException {
 		
 		HikeSharedPreferenceUtil sharedPref = HikeSharedPreferenceUtil.getInstance(ctx);
-		String key = sharedPref.getData(HikeMessengerApp.ACCOUNT_SETTINGS, "");	
+		String key = sharedPref.getData(HikeMessengerApp.ACCOUNT_SETTINGS, null);	
 		//for the case when AI packet will not send us the backup Token
-		String salt = sharedPref.getData(HikeMessengerApp.BACKUP_TOKEN_SETTING, "");
+		String salt = sharedPref.getData(HikeMessengerApp.BACKUP_TOKEN_SETTING, null);
 		// if salt or key is empty, we do not send anything
-		if(salt.equals("") || key.equals(""))
+		if(salt == null|| key == null)
 			return null;
 		
 		AESEncryption aesObj = new AESEncryption(key + salt, HASH_SCHEME);
@@ -176,17 +181,19 @@ public class UserLogInfo {
 
 	}
 	
-	private static JSONArray getJSONLogArray(Context ctx, int flag) throws JSONException{	
-		JSONArray jsonLogArray = null;
+	private static JSONArray collectLogs(Context ctx, int flag) throws JSONException{	
 		switch(flag){
-			case APP_ANALYTICS_FLAG : jsonLogArray = getJSONAppArray(getAppLogs(ctx)); break;
+			case APP_ANALYTICS_FLAG : return getJSONAppArray(getAppLogs(ctx)); 
 			//case CALL_ANALYTICS_FLAG : jsonLogArray = getJSONCallArray(getCallLogs(ctx)); break;
-			case LOCATION_ANALYTICS_FLAG : jsonLogArray = getJSONLocArray(getLocLogs(ctx)); break;	
+			case LOCATION_ANALYTICS_FLAG : return getJSONLocArray(getLocLogs(ctx));
+			default : return null;
 		}
-		return jsonLogArray;
 	}
 	
 	private static JSONArray getJSONLocArray(List<LocLogPojo> locLogList) throws JSONException{
+		//not sending anything if there is no Log available
+		if(locLogList == null)
+			return null;
 		JSONArray locJsonArray = new JSONArray();
 		for(LocLogPojo locLog : locLogList){
 			locJsonArray.put(locLog.toJSON());
@@ -198,16 +205,22 @@ public class UserLogInfo {
 	private static List<LocLogPojo> getLocLogs(Context ctx){
 		Location bestLocation = null;
 		LocationManager locManager = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+		List<String> locProviders = locManager.getProviders(true);
+		if (locProviders == null || locProviders.isEmpty())
+			return null;
 		for(String provider : locManager.getProviders(true)){
 			Location location = locManager.getLastKnownLocation(provider);
 			if(location == null)
 				continue;
-			if (bestLocation == null || location.getAccuracy() < bestLocation.getAccuracy()){
+			if (bestLocation == null || 
+					(location.hasAccuracy() && location.getAccuracy() < bestLocation.getAccuracy())){
 				bestLocation = location;
 			}
 		}
-		LocLogPojo locLog = new LocLogPojo(bestLocation.getLatitude(), 
-				bestLocation.getLongitude(), bestLocation.getAccuracy());
+		if(bestLocation == null)
+			return null;
+		LocLogPojo locLog = new LocLogPojo(bestLocation.getLatitude(), bestLocation.getLongitude(), 
+				bestLocation.getAccuracy(), bestLocation.getTime());
 		List<LocLogPojo> locLogList = new ArrayList<LocLogPojo>(1);
 		locLogList.add(locLog);
 		return locLogList;
@@ -215,7 +228,7 @@ public class UserLogInfo {
 
 	public static void sendLogs(Context ctx, int flags) throws JSONException {
 		
-		JSONArray jsonLogArray = getJSONLogArray(ctx, flags);	
+		JSONArray jsonLogArray = collectLogs(ctx, flags);	
 		// if nothing is logged we do not send anything
 		if(jsonLogArray != null){		
 			JSONObject jsonLogObj = getEncryptedJSON(ctx, jsonLogArray, flags);
