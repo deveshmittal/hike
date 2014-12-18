13edbdc21319f0bd473f3f6f50a460abea536cda
Rajesh <rajesh@bsb.in>
2014-12-18 14:26:57 +0530

    AnalyticsSettings class deleted. Code moved to HAManager.

--- src/com/bsb/hike/analytics/HAManager.java
+++ src/com/bsb/hike/analytics/HAManager.java
@@ -3,6 +3,8 @@ package com.bsb.hike.analytics;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 
 import com.bsb.hike.utils.Logger;
 
@@ -30,6 +32,8 @@ public class HAManager
 	
 	private Thread writerThread;
 	
+	private boolean isAnalyticsEnabled = true;
+	
 	/**
 	 * Constructor
 	 */
@@ -39,7 +43,9 @@ public class HAManager
 		
 		eventsList = new ArrayList<Event>();
 		
-		store = new AnalyticsStore(this.context);					
+		store = AnalyticsStore.getInstance(this.context);	
+		
+		isAnalyticsEnabled = getPrefs().getBoolean(HAManager.ANALYTICS_SERVICE_STATUS, AnalyticsConstants.IS_ANALYTICS_ENABLED);
 	}
 	
 	/**
@@ -88,8 +94,57 @@ public class HAManager
 	 * @param whenToSend time(long) of the day when event file should be sent to the server
 	 * @param isServiceEnabled true if analytics service should be stopped, false otherwise
 	 */
-	public static void configureAnalyticsService(Context context, long maxFileSize, int whenToSend, boolean isAnalyticsEnabled)	
+	public void configureAnalyticsService(long maxFileSize, int whenToSend, boolean isServiceEnabled)	
 	{
-		AnalyticsSettings.configureAnalyticsService(context, maxFileSize, whenToSend, isAnalyticsEnabled);
+		Editor editor = getPrefs().edit();
+		editor.putLong(HAManager.FILE_SIZE_LIMIT, AnalyticsConstants.MAX_FILE_SIZE);
+		editor.putBoolean(HAManager.ANALYTICS_SERVICE_STATUS, AnalyticsConstants.IS_ANALYTICS_ENABLED);
+		editor.putInt(HAManager.HOUR_TO_SEND, AnalyticsConstants.HOUR_OF_DAY_TO_SEND);
+		editor.commit();
+	}
+	
+	/**
+	 * Returns current max log file size 
+	 * @return log file size in bytes
+	 */
+	public long getMaxFileSize()	
+	{
+		return getPrefs().getLong(HAManager.FILE_SIZE_LIMIT, AnalyticsConstants.MAX_FILE_SIZE );
+	}
+	
+	/**
+	 * Returns the hour of the day when log file should be sent to the server
+	 * @return hour of the day(0-23)
+	 */
+	public int getWhenToSend()
+	{
+		return getPrefs().getInt(HAManager.HOUR_TO_SEND, AnalyticsConstants.HOUR_OF_DAY_TO_SEND);
+	}
+	
+	/**
+	 * Returns whether analytics logging service is currently enabled or disabled 
+	 * @return true if logging service is enabled, false otherwise
+	 */
+	public boolean isAnalyticsEnabled()
+	{
+		return isAnalyticsEnabled;
+	}
+	
+	/**
+	 * Used to enable/disable the analytics service
+	 * @param isAnalyticsEnabled true if analytics service is enabled, false otherwise
+	 */
+	public void setAnalyticsEnabled(boolean isAnalyticsEnabled)
+	{
+		this.isAnalyticsEnabled = isAnalyticsEnabled;
+	}
+	
+	/**
+	 * Used to get the application's SharedPreferences
+	 * @return SharedPreference of the application
+	 */
+	private SharedPreferences getPrefs()
+	{
+		return context.getSharedPreferences(HAManager.ANALYTICS_SETTINGS, Context.MODE_PRIVATE);		
 	}
 }
