57238700e7d6e63b404f23fca8ca31a2d2173acd
Rajesh <rajesh@bsb.in>
2014-12-19 16:51:01 +0530

    Setting alarm to send analytics data in case phone is rebooted.

--- src/com/bsb/hike/service/BootService.java
+++ src/com/bsb/hike/service/BootService.java
@@ -1,11 +1,16 @@
 package com.bsb.hike.service;
 
+import java.util.Calendar;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.text.TextUtils;
 
 import com.bsb.hike.HikeMessengerApp;
+import com.bsb.hike.analytics.AnalyticsSender;
+import com.bsb.hike.analytics.HAManager;
+import com.bsb.hike.models.HikeAlarmManager;
 import com.bsb.hike.utils.HikeSharedPreferenceUtil;
 import com.bsb.hike.utils.Logger;
 import com.bsb.hike.utils.Utils;
@@ -28,9 +33,12 @@ public class BootService extends BroadcastReceiver
 		if (TextUtils.isEmpty(mprefs.getData(HikeMessengerApp.TOKEN_SETTING, null)))
 		{
 			return;
-		}
+		}		
 		Intent startServiceIntent = new Intent(ctx, HikeService.class);
 		ctx.startService(startServiceIntent);
+		
+		long whenToSend = Utils.getTimeInMillis(Calendar.getInstance(), HAManager.getInstance(ctx).getWhenToSend(), 0, 0);
+		HikeAlarmManager.setAlarm(ctx, whenToSend, HikeAlarmManager.REQUESTCODE_HIKE_ANALYTICS, true);
 	}
 
 }
