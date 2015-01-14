f0a1b73ccfd6c78da66a2eafbe6f07ce1560e02b
gauravKhanna <gaurav@bsb.in>
2015-01-14 13:32:44 +0530

    added function to get metadata of message and update helper data

--- src/com/bsb/hike/HikeConstants.java
+++ src/com/bsb/hike/HikeConstants.java
@@ -2099,4 +2099,6 @@ public class HikeConstants
 	public static final String GCM_ID = "gcm_id";
 	
 	public static final String ADD_CATEGORY = "addCat";
+	
+	public static final String HELPER_DATA = "helper_data";
 }
