603f6f6f081076827aa14806982aaab9595260ce
piyushghai <piyush@hike.in>
2015-01-21 16:47:23 +0530

    Formatting changes in AttachmentPicker

--- src/com/bsb/hike/media/AttachmentPicker.java
+++ src/com/bsb/hike/media/AttachmentPicker.java
@@ -21,19 +21,27 @@ import com.bsb.hike.R;
 import com.bsb.hike.utils.IntentFactory;
 import com.bsb.hike.utils.Logger;
 
-public class AttachmentPicker extends OverFlowMenuLayout {
+public class AttachmentPicker extends OverFlowMenuLayout
+{
 
 	private static final String tag = "attachmentPicker";
 
 	public static final int CAMERA = 313;
+
 	public static final int GALLERY = 314;
+
 	public static final int AUDIO = 315;
+
 	public static final int VIDEO = 316;
+
 	public static final int FILE = 317;
+
 	public static final int CONTACT = 318;
+
 	public static final int LOCATOIN = 319;
 
 	private boolean startRespectiveActivities;
+
 	private Activity activity;
 
 	/**
@@ -42,88 +50,78 @@ public class AttachmentPicker extends OverFlowMenuLayout {
 	 * @param listener
 	 * @param context
 	 * @param startRespectiveActivities
-	 *            - if true, we will start respective activities on activity
-	 *            behalf and activity has to handle onActivityResult callback
-	 *            where request code is Overflowitem uniqueness
+	 *            - if true, we will start respective activities on activity behalf and activity has to handle onActivityResult callback where request code is Overflowitem
+	 *            uniqueness
 	 */
-	public AttachmentPicker(List<OverFlowMenuItem> overflowItems,
-			OverflowItemClickListener listener, Context context,
-			boolean startRespectiveActivities) {
+	public AttachmentPicker(List<OverFlowMenuItem> overflowItems, OverflowItemClickListener listener, Context context, boolean startRespectiveActivities)
+	{
 		super(overflowItems, listener, context);
 		this.startRespectiveActivities = startRespectiveActivities;
 	}
 
 	/**
-	 * By default we show {@link #CAMERA} {@link #GALLARY} {@link #AUDIO}
-	 * {@link #VIDEO} {@link #FILE} {@link #CONTACT} {@link #LOCATOIN}
+	 * By default we show {@link #CAMERA} {@link #GALLARY} {@link #AUDIO} {@link #VIDEO} {@link #FILE} {@link #CONTACT} {@link #LOCATOIN}
 	 * 
 	 * @param listener
 	 * @param context
 	 * @param startRespectiveActivities
-	 *            - if true, we will start respective activities on activity
-	 *            behalf and activity has to handle onActivityResult callback
-	 *            and request code will be constants given above
+	 *            - if true, we will start respective activities on activity behalf and activity has to handle onActivityResult callback and request code will be constants given
+	 *            above
 	 */
-	public AttachmentPicker(OverflowItemClickListener listener,
-			Activity activity, boolean startRespectiveActivities) {
-		this(null, listener, activity.getApplicationContext(),
-				startRespectiveActivities);
+	public AttachmentPicker(OverflowItemClickListener listener, Activity activity, boolean startRespectiveActivities)
+	{
+		this(null, listener, activity.getApplicationContext(), startRespectiveActivities);
 		this.activity = activity;
 		initDefaultAttachmentList();
 	}
 
-	private void initDefaultAttachmentList() {
+	private void initDefaultAttachmentList()
+	{
 		List<OverFlowMenuItem> items = new ArrayList<OverFlowMenuItem>(7);
-		items.add(new OverFlowMenuItem(getString(R.string.camera_upper_case),
-				0, R.drawable.ic_attach_camera, CAMERA));
-		items.add(new OverFlowMenuItem(getString(R.string.photo), 0,
-				R.drawable.ic_attach_pic, GALLERY));
-		items.add(new OverFlowMenuItem(getString(R.string.audio), 0,
-				R.drawable.ic_attach_music, AUDIO));
-		items.add(new OverFlowMenuItem(getString(R.string.video), 0,
-				R.drawable.ic_attach_video, VIDEO));
-		items.add(new OverFlowMenuItem(getString(R.string.file), 0,
-				R.drawable.ic_attach_file, FILE));
-		if (context.getPackageManager().hasSystemFeature(
-				PackageManager.FEATURE_LOCATION)) {
-			items.add(new OverFlowMenuItem(getString(R.string.location_option),
-					0, R.drawable.ic_attach_location, LOCATOIN));
+		items.add(new OverFlowMenuItem(getString(R.string.camera_upper_case), 0, R.drawable.ic_attach_camera, CAMERA));
+		items.add(new OverFlowMenuItem(getString(R.string.photo), 0, R.drawable.ic_attach_pic, GALLERY));
+		items.add(new OverFlowMenuItem(getString(R.string.audio), 0, R.drawable.ic_attach_music, AUDIO));
+		items.add(new OverFlowMenuItem(getString(R.string.video), 0, R.drawable.ic_attach_video, VIDEO));
+		items.add(new OverFlowMenuItem(getString(R.string.file), 0, R.drawable.ic_attach_file, FILE));
+		if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_LOCATION))
+		{
+			items.add(new OverFlowMenuItem(getString(R.string.location_option), 0, R.drawable.ic_attach_location, LOCATOIN));
 		}
 		this.overflowItems = items;
 	}
 
 	@Override
-	public View getView() {
+	public View getView()
+	{
 		return viewToShow;
 	}
 
 	@Override
-	public void initView() {
+	public void initView()
+	{
 		// we lazily inflate and
-		if (viewToShow != null) {
+		if (viewToShow != null)
+		{
 			return;
 		}
 
-		View parentView = viewToShow = LayoutInflater.from(context).inflate(
-				R.layout.attachments, null);
+		View parentView = viewToShow = LayoutInflater.from(context).inflate(R.layout.attachments, null);
 
-		GridView attachmentsGridView = (GridView) parentView
-				.findViewById(R.id.attachment_grid);
-		attachmentsGridView.setAdapter(new ArrayAdapter<OverFlowMenuItem>(
-				context, R.layout.attachment_item, R.id.text, overflowItems) {
+		GridView attachmentsGridView = (GridView) parentView.findViewById(R.id.attachment_grid);
+		attachmentsGridView.setAdapter(new ArrayAdapter<OverFlowMenuItem>(context, R.layout.attachment_item, R.id.text, overflowItems)
+		{
 
 			@Override
-			public View getView(int position, View convertView, ViewGroup parent) {
-				if (convertView == null) {
-					convertView = LayoutInflater.from(context).inflate(
-							R.layout.attachment_item, parent, false);
+			public View getView(int position, View convertView, ViewGroup parent)
+			{
+				if (convertView == null)
+				{
+					convertView = LayoutInflater.from(context).inflate(R.layout.attachment_item, parent, false);
 				}
 				OverFlowMenuItem menuItem = getItem(position);
 
-				ImageView attachmentImageView = (ImageView) convertView
-						.findViewById(R.id.attachment_icon);
-				TextView attachmentTextView = (TextView) convertView
-						.findViewById(R.id.text);
+				ImageView attachmentImageView = (ImageView) convertView.findViewById(R.id.attachment_icon);
+				TextView attachmentTextView = (TextView) convertView.findViewById(R.id.text);
 
 				attachmentImageView.setImageResource(menuItem.drawableId);
 				attachmentTextView.setText(menuItem.text);
@@ -132,15 +130,17 @@ public class AttachmentPicker extends OverFlowMenuLayout {
 			}
 		});
 
-		attachmentsGridView.setOnItemClickListener(new OnItemClickListener() {
+		attachmentsGridView.setOnItemClickListener(new OnItemClickListener()
+		{
 
 			@Override
-			public void onItemClick(AdapterView<?> adapterView, View view,
-					int position, long id) {
+			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
+			{
 				popUpLayout.dismiss();
 
 				OverFlowMenuItem item = overflowItems.get(position);
-				if (!startRespectiveActivities) {
+				if (!startRespectiveActivities)
+				{
 					listener.itemClicked(item);
 					return;
 				}
@@ -148,7 +148,8 @@ public class AttachmentPicker extends OverFlowMenuLayout {
 				// Start respective activities
 				int requestCode = -1;
 				Intent pickIntent = null;
-				switch (item.id) {
+				switch (item.id)
+				{
 				case CAMERA:
 					requestCode = CAMERA;
 					pickIntent = IntentFactory.getImageCaptureIntent(context);
@@ -177,9 +178,12 @@ public class AttachmentPicker extends OverFlowMenuLayout {
 					listener.itemClicked(item);
 					break;
 				}
-				if (pickIntent != null) {
+				if (pickIntent != null)
+				{
 					activity.startActivityForResult(pickIntent, requestCode);
-				} else {
+				}
+				else
+				{
 					Logger.e(tag, "intent is null !!");
 				}
 			}
@@ -187,7 +191,8 @@ public class AttachmentPicker extends OverFlowMenuLayout {
 
 	}
 
-	private String getString(int id) {
+	private String getString(int id)
+	{
 		return context.getString(id);
 	}
 }
