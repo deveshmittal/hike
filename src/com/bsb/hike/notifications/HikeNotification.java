f2078de88ba4f2c8aaaf2071436de8af1bac097f
Rohit Singhal <rohit@hoppr.com>
2015-01-23 11:05:11 +0530

    Adding Mode check to stop sound + vibrate in call

--- src/com/bsb/hike/notifications/HikeNotification.java
+++ src/com/bsb/hike/notifications/HikeNotification.java
@@ -1106,7 +1106,8 @@ public class HikeNotification
 		if (!forceNotPlaySound)
 		{
 			final boolean shouldNotPlayNotification = (System.currentTimeMillis() - lastNotificationTime) < MIN_TIME_BETWEEN_NOTIFICATIONS;
-			if (!shouldNotPlayNotification)
+			if (!shouldNotPlayNotification && ((manager.getMode() != AudioManager.MODE_IN_COMMUNICATION 
+					|| manager.getMode() != AudioManager.MODE_IN_CALL)))
 			{
 				String notifSound = HikeSharedPreferenceUtil.getInstance(context).getData(HikeConstants.NOTIF_SOUND_PREF, NOTIF_SOUND_HIKE);
 				Logger.i("notif", "sound " + notifSound);
