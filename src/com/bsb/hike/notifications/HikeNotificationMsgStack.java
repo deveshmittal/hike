ec2f778c4e6c08036524c3ba0160ef29b67f5448
gauravKhanna <gaurav@bsb.in>
2015-02-06 12:45:13 +0530

    logging error and returning, earlier app was crashing

--- src/com/bsb/hike/notifications/HikeNotificationMsgStack.java
+++ src/com/bsb/hike/notifications/HikeNotificationMsgStack.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.text.SpannableString;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Pair;
 
 import com.bsb.hike.HikeConstants;
@@ -161,7 +162,8 @@ public class HikeNotificationMsgStack implements Listener
 	{
 		if (TextUtils.isEmpty(argMessage))
 		{
-			throw new IllegalArgumentException("argMessage cannot be null or empty");
+			Log.wtf("HikeNotification", "Notification message is empty, check packet, msisdn= "+argMsisdn);
+			return;
 		}
 		addPair(argMsisdn, argMessage);
 	}
