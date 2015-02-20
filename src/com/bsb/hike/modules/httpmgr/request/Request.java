a89208eb61793fbf44e9f6092c3e0d9702321400
Sidharth <sidharth@hike.in>
2015-02-20 12:44:57 +0530

    minor change in request one listener will be attached to one request token

--- src/com/bsb/hike/modules/httpmgr/request/Request.java
+++ src/com/bsb/hike/modules/httpmgr/request/Request.java
@@ -79,7 +79,7 @@ public abstract class Request<T> implements IRequestFacade
 		this.priority = builder.priority;
 		this.requestType = builder.requestType;
 		this.retryPolicy = builder.retryPolicy;
-		this.requestListeners = builder.requestListeners;
+		addRequestListeners(builder.requestListeners);
 		this.responseOnUIThread = builder.responseOnUIThread;
 		this.asynchronous = builder.asynchronous;
 		ensureSaneDefaults();
@@ -372,6 +372,15 @@ public abstract class Request<T> implements IRequestFacade
 		this.isCancelled = isCancelled;
 	}
 
+	public void addRequestListeners(IRequestListener requestListener)
+	{
+		if (this.requestListeners == null)
+		{
+			this.requestListeners = new CopyOnWriteArrayList<IRequestListener>();
+		}
+		this.requestListeners.add(requestListener);
+	}
+	
 	/**
 	 * add list of listeners to existing list of request listeners
 	 * @param requestListeners
@@ -477,7 +486,7 @@ public abstract class Request<T> implements IRequestFacade
 
 		private IRetryPolicy retryPolicy = new DefaultRetryPolicy();
 
-		private CopyOnWriteArrayList<IRequestListener> requestListeners;
+		private IRequestListener requestListeners;
 
 		private boolean responseOnUIThread;
 
@@ -624,11 +633,7 @@ public abstract class Request<T> implements IRequestFacade
 		 */
 		public S setRequestListener(IRequestListener requestListener)
 		{
-			if (this.requestListeners == null)
-			{
-				this.requestListeners = new CopyOnWriteArrayList<IRequestListener>();
-			}
-			this.requestListeners.add(requestListener);
+			this.requestListeners = requestListener;
 			return self();
 		}
 
