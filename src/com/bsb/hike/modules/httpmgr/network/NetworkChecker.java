9ec406087745749d98ff186174463b786cee9385
Anubhav <anubhav.7nov@gmail.com>
2015-02-24 13:39:15 +0530

    modified is nework available function in nw checker

--- src/com/bsb/hike/modules/httpmgr/network/NetworkChecker.java
+++ src/com/bsb/hike/modules/httpmgr/network/NetworkChecker.java
@@ -1,10 +1,12 @@
 package com.bsb.hike.modules.httpmgr.network;
 
+import com.bsb.hike.HikeMessengerApp;
+import com.bsb.hike.utils.Utils;
+
 public class NetworkChecker
 {
 	public static boolean isNetworkAvailable()
 	{
-		// TODO
-		return true;
+		return Utils.isUserOnline(HikeMessengerApp.getInstance());
 	}
 }
