8d93a9f7dca2496181096dfda07dfdaa0fedce44
Sidharth <sidharth@hike.in>
2015-02-25 18:01:54 +0530

    added threshold lenght for gzipping
    Conflicts:
    	src/com/bsb/hike/modules/httpmgr/interceptor/GzipRequestInterceptor.java

--- src/com/bsb/hike/modules/httpmgr/interceptor/GzipRequestInterceptor.java
+++ src/com/bsb/hike/modules/httpmgr/interceptor/GzipRequestInterceptor.java
@@ -7,18 +7,24 @@ import com.bsb.hike.modules.httpmgr.request.requestbody.GzipRequestBody;
 
 /** This interceptor compresses the HTTP request body */
 public final class GzipRequestInterceptor implements IRequestInterceptor
-{
+{ 
+	private static final int THRESHOLD_LENGTH_FOR_GZIP = 250;
+	
 	@Override
 	public void intercept(Chain chain)
 	{
 		RequestFacade requestFacade = chain.getRequestFacade();
 
-		if (requestFacade.getBody() == null || Utils.containsHeader(requestFacade.getHeaders(),"Content-Encoding"))
+		if (requestFacade.getBody() == null || Utils.containsHeader(requestFacade.getHeaders(), "Content-Encoding"))
 		{
 			chain.proceed();
 		}
-		requestFacade.getHeaders().add(new Header("Content-Encoding", "gzip"));
-		requestFacade.setBody(new GzipRequestBody(requestFacade.getBody()));
+
+		if (requestFacade.getBody().length() >= THRESHOLD_LENGTH_FOR_GZIP)
+		{
+			requestFacade.getHeaders().add(new Header("Content-Encoding", "gzip"));
+			requestFacade.setBody(new GzipRequestBody(requestFacade.getBody()));
+		}
 		chain.proceed();
 	}
 }
