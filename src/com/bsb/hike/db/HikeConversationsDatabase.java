3387f85d47e113b89e41c9f30355b55036d2505c
Anu Bansal <anu@hike.in>
2015-03-05 19:56:16 +0530

    moved methods for adding 1-1 recipients and sendPubSubForConvScreen to dbConversationListener

--- src/com/bsb/hike/db/HikeConversationsDatabase.java
+++ src/com/bsb/hike/db/HikeConversationsDatabase.java
@@ -1459,56 +1459,6 @@ public class HikeConversationsDatabase extends SQLiteOpenHelper implements DBCon
 		return msgHash;
 	}
 	
-	public void addBroadcastRecipientConversations(ConvMessage convMessage)
-	{
-		ContactManager contactManager = ContactManager.getInstance();
-		ArrayList<ConvMessage> convMessages= new ArrayList<ConvMessage>(1);
-		ConvMessage broadcastConvMessage = new ConvMessage(convMessage);
-		//Here we set origin type of 
-		broadcastConvMessage.setMessageOriginType(ConvMessage.OriginType.BROADCAST);
-		convMessages.add(broadcastConvMessage);
-		ArrayList<String> contactMsisdns = convMessage.getSentToMsisdnsList();
-		ArrayList<ContactInfo> contacts = new ArrayList<ContactInfo>();
-		for (String msisdn : contactMsisdns)
-		{
-			ContactInfo contactInfo = new ContactInfo(msisdn, msisdn, contactManager.getName(msisdn), msisdn, true);
-			contacts.add(contactInfo);
-		}
-		
-		addConversations(convMessages, contacts, false);
-		
-		sendPubSubForConvScreenBroadcastMessage(convMessage, contacts);
-        // publishing mqtt packet
-        HikeMqttManagerNew.getInstance().sendMessage(convMessage.serializeDeliveryReportRead(), HikeMqttManagerNew.MQTT_QOS_ONE);
-	}
-	
-	public void sendPubSubForConvScreenBroadcastMessage(ConvMessage convMessage, ArrayList<ContactInfo> recipient)
-	{
-		long msgId = convMessage.getMsgID();
-		int totalRecipient = recipient.size();
-		List<Pair<ContactInfo, ConvMessage>> allPairs = new ArrayList<Pair<ContactInfo,ConvMessage>>(totalRecipient);
-		long timestamp = System.currentTimeMillis()/1000;
-		for(int i=0;i<totalRecipient;i++)
-		{
-			ConvMessage message = new ConvMessage(convMessage);
-			if(convMessage.isBroadcastConversation())
-			{
-				message.setMessageOriginType(OriginType.BROADCAST);
-			}
-			else
-			{
-				//multi-forward case... in braodcast case we donot need to update timestamp
-				message.setTimestamp(timestamp++);
-			}
-			message.setMsgID(msgId+i);
-			ContactInfo contactInfo = recipient.get(i);
-			message.setMsisdn(contactInfo.getMsisdn());
-			Pair<ContactInfo, ConvMessage> pair = new Pair<ContactInfo, ConvMessage>(contactInfo, message);
-			allPairs.add(pair);
-		}
-		HikeMessengerApp.getPubSub().publish(HikePubSub.MULTI_MESSAGE_DB_INSERTED, allPairs);
-	}
-	
 	public boolean addConversations(List<ConvMessage> convMessages)
 	{
 		if(convMessages.isEmpty())
