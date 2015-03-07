869bc0e96c00c2768d6e4125d25a1996c6cde201
Shobhit <shobhit@Shobhits-MacBook-Pro.local>
2015-03-07 12:23:25 +0530

    request added for microApp zip download

--- src/com/bsb/hike/modules/httpmgr/hikehttp/HttpRequests.java
+++ src/com/bsb/hike/modules/httpmgr/hikehttp/HttpRequests.java
@@ -3,8 +3,11 @@ package com.bsb.hike.modules.httpmgr.hikehttp;
 import static com.bsb.hike.modules.httpmgr.hikehttp.HttpRequestConstants.singleStickerDownloadBase;
 
 import com.bsb.hike.modules.httpmgr.RequestToken;
+import com.bsb.hike.modules.httpmgr.request.FileRequest;
 import com.bsb.hike.modules.httpmgr.request.JSONObjectRequest;
 import com.bsb.hike.modules.httpmgr.request.listener.IRequestListener;
+import com.bsb.hike.modules.httpmgr.retry.DefaultRetryPolicy;
+import com.bsb.hike.platform.content.PlatformContentConstants;
 import com.bsb.hike.utils.Utils;
 
 public class HttpRequests
@@ -14,6 +17,17 @@ public class HttpRequests
 		RequestToken requestToken = new JSONObjectRequest.Builder()
 				.setUrl(singleStickerDownloadBase() + "?catId=" + categoryId + "&stId=" + stickerId + "&resId=" + Utils.getResolutionId())
 				.setRequestListener(requestListener).build();
+		return requestToken;
+	}
+
+	public static RequestToken platformZipDownloadRequest(String url, IRequestListener requestListener)
+	{
+		RequestToken requestToken = new FileRequest.Builder()
+				.setUrl(url)
+				.setFile(PlatformContentConstants.PLATFORM_CONTENT_DIR + PlatformContentConstants.TEMP_DIR_NAME)
+				.setRequestListener(requestListener)
+				.setRetryPolicy(new DefaultRetryPolicy(3, 1000, 2.0f))
+				.build();
 		return requestToken;
 	}
 }
