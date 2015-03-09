5d5c2b7df56c232724db7b78cd84d1634c5f9716
Rohit Singhal <rohit@hoppr.com>
2015-03-09 14:36:41 +0530

    Sending Bg Packet on Device Shutdown only when app is in foreground

--- src/com/bsb/hike/service/DeviceShutDownReceiver.java
+++ src/com/bsb/hike/service/DeviceShutDownReceiver.java
@@ -3,6 +3,8 @@ package com.bsb.hike.service;
 import org.json.JSONObject;
 
 import com.bsb.hike.HikeConstants;
+import com.bsb.hike.HikeMessengerApp;
+import com.bsb.hike.HikeMessengerApp.CurrentState;
 import com.bsb.hike.analytics.HAManager;
 import com.bsb.hike.utils.Utils;
 
@@ -16,9 +18,14 @@ public class DeviceShutDownReceiver extends BroadcastReceiver
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
-		//Send "bg" packet to MQTT with QOS '1'
-		JSONObject sessionDataObject = HAManager.getInstance().recordAndReturnSessionEnd();
-		Utils.sendSessionMQTTPacket(context, HikeConstants.BACKGROUND, sessionDataObject);
+		// Send MQTT bg packet only when App is not in Background
+		if(!(HikeMessengerApp.currentState == CurrentState.CLOSED
+				|| HikeMessengerApp.currentState == CurrentState.BACKGROUNDED))
+		{
+			//Send "bg" packet to MQTT with QOS '1'
+			JSONObject sessionDataObject = HAManager.getInstance().recordAndReturnSessionEnd();
+			Utils.sendSessionMQTTPacket(context, HikeConstants.BACKGROUND, sessionDataObject);
+		}
 	}
 
 }
