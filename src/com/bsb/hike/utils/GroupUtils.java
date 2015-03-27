e6fc29de4fe46072064de11c90f83dace87238a0
piyushghai <piyush@hike.in>
2015-03-27 12:21:37 +0530

    Moved methods to newer method names

--- src/com/bsb/hike/utils/GroupUtils.java
+++ src/com/bsb/hike/utils/GroupUtils.java
@@ -3,11 +3,10 @@ package com.bsb.hike.utils;
 import android.content.Context;
 
 import com.bsb.hike.R;
-import com.bsb.hike.models.BroadcastConversation;
 import com.bsb.hike.models.ConvMessage;
-import com.bsb.hike.models.Conversation;
-import com.bsb.hike.models.GroupConversation;
 import com.bsb.hike.models.MessageMetadata;
+import com.bsb.hike.models.Conversation.BroadcastConversation;
+import com.bsb.hike.models.Conversation.OneToNConversation;
 
 public class GroupUtils {
 
@@ -40,19 +39,19 @@ public class GroupUtils {
 		return participantAddedMessage;
 	}
 	
-	public static String getParticipantRemovedMessage(Conversation conversation, Context context, String participantName)
+	public static String getParticipantRemovedMessage(OneToNConversation conversation, Context context, String participantName)
 	{
 		String participantRemovedMessage = String.format(context.getString(conversation instanceof BroadcastConversation ? R.string.removed_from_broadcast : R.string.left_conversation), participantName);
 		return participantRemovedMessage;
 	}
 	
-	public static String getConversationNameChangedMessage(Conversation conversation, Context context, String participantName)
+	public static String getConversationNameChangedMessage(OneToNConversation conversation, Context context, String participantName)
 	{
 		String nameChangedMessage = String.format(context.getString(conversation instanceof BroadcastConversation ? R.string.change_broadcast_name : R.string.change_group_name), participantName);
 		return nameChangedMessage;
 	}
 	
-	public static String getConversationEndedMessage(Conversation conversation, Context context)
+	public static String getConversationEndedMessage(OneToNConversation conversation, Context context)
 	{
 		String message = context.getString(conversation instanceof BroadcastConversation ? R.string.broadcast_list_end : R.string.group_chat_end);
 		return message;
