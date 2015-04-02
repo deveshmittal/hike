581dae4eeeb7ba3dd9b6a69366ad735319c2be10
Anu Bansal <anu@hike.in>
2015-04-02 17:54:53 +0530

    Added nudge disable and overflow items in Broadcast ChatThread

--- src/com/bsb/hike/chatthread/BroadcastChatThread.java
+++ src/com/bsb/hike/chatthread/BroadcastChatThread.java
@@ -3,6 +3,14 @@
  */
 package com.bsb.hike.chatthread;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.view.MotionEvent;
+
+import com.bsb.hike.R;
+import com.bsb.hike.media.OverFlowMenuItem;
+
 /**
  * @author piyush
  * 
@@ -31,4 +39,24 @@ public class BroadcastChatThread extends OneToNChatThread
 		return null;
 	}
 
+	@Override
+	public boolean onDoubleTap(MotionEvent e)
+	{
+		return false;
+	}
+	
+	private List<OverFlowMenuItem> getOverFlowItems()
+	{
+
+		List<OverFlowMenuItem> list = new ArrayList<OverFlowMenuItem>();
+		list.add(new OverFlowMenuItem(getString(R.string.broadcast_profile), 0, 0, R.string.broadcast_profile));
+
+		for (OverFlowMenuItem item : super.getOverFlowMenuItems())
+		{
+			list.add(item);
+		}
+		list.add(new OverFlowMenuItem(getString(R.string.add_shortcut), 0, 0, R.string.add_shortcut));
+		
+		return list;
+	}
 }
