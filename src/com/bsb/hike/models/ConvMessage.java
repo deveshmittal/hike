93a0fef050ad7596ad140264dbe1381a95ee3be8
piyushghai <piyush@hike.in>
2015-04-06 13:11:04 +0530

    Removed compilation errors

--- src/com/bsb/hike/models/ConvMessage.java
+++ src/com/bsb/hike/models/ConvMessage.java
@@ -928,7 +928,7 @@ public class ConvMessage implements Searchable
 		{
 			object.put(HikeConstants.MESSAGE_ID, Long.toString(System.currentTimeMillis()/1000));
 			object.put(HikeConstants.TO, mMsisdn);
-			if(privateData != null && privateData.getTrackID() != null && !Utils.isGroupConversation(mMsisdn))
+			if(privateData != null && privateData.getTrackID() != null && !OneToNConversationUtils.isGroupConversation(mMsisdn))
 			{
 				// "d":{"msgid1":{track_id:"value"}}
 				JSONObject obj = new JSONObject();
